d975da418267e9fbb34c5a82b95efcc6e47dbcfd
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-04-16 18:54:01 +0200

    add user support

--- smb_config/run.sh
+++ smb_config/run.sh
@@ -8,14 +8,19 @@ GUEST=$(jq --raw-output ".guest // empty" $CONFIG_PATH)
 USERNAME=$(jq --raw-output ".username // empty" $CONFIG_PATH)
 PASSWORD=$(jq --raw-output ".password // empty" $CONFIG_PATH)
 
-sed -i "/workgroup =.*/workgroup = $WORKGROUP/" /etc/smb.conf
+sed -i "/%%WORKGROUP%%/$WORKGROUP/" /etc/smb.conf
 
 if [ GUEST == "true" ];
-    sed -i "/group ok =.*/group ok = yes/" /etc/smb.conf
-    sed -i "/public =.*/public = yes/" /etc/smb.conf
+    echo "  group ok = yes" >> /etc/smb.conf
+    echo "  public = yes" >> /etc/smb.conf
 else
-    sed -i "/group ok =.*/group ok = no/" /etc/smb.conf
-    sed -i "/public =.*/public = no/" /etc/smb.conf
+    echo "  valid users = $USERNAME" >> /etc/smb.conf
+    echo "  force user = root" >> /etc/smb.conf
+    echo "  force group = root" >> /etc/smb.conf
+
+    addgroup -g 1000 $USERNAME
+    adduser -D -H -G $USERNAME -s /bin/false -u 1000 $USERNAME
+    echo -e "$PASSWORD\n$PASSWORD" | smbpasswd -a -s -c /etc/smb.conf $USERNAME
 fi
 
 smbd -F -S -s /etc/smb.conf

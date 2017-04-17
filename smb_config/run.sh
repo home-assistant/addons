d80b9b0ce62ce133a0e4f9c47ba1197aec4289c1
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-04-18 00:12:21 +0200

    update addon version 0.3

--- smb_config/run.sh
+++ smb_config/run.sh
@@ -11,12 +11,14 @@ PASSWORD=$(jq --raw-output ".password // empty" $CONFIG_PATH)
 sed -i "s/%%WORKGROUP%%/$WORKGROUP/g" /etc/smb.conf
 
 if [ $GUEST == "true" ]; then
-    echo "  guest ok = yes" >> /etc/smb.conf
-    echo "  public = yes" >> /etc/smb.conf
+    echo "   guest ok = yes" >> /etc/smb.conf
+    echo "   guest only = yes" >> /etc/smb.conf
+    echo "   guest account = root" >> /etc/smb.conf
+    echo "   public = yes" >> /etc/smb.conf
 else
-    echo "  valid users = $USERNAME" >> /etc/smb.conf
-    echo "  force user = root" >> /etc/smb.conf
-    echo "  force group = root" >> /etc/smb.conf
+    echo "   valid users = $USERNAME" >> /etc/smb.conf
+    echo "   force user = root" >> /etc/smb.conf
+    echo "   force group = root" >> /etc/smb.conf
 
     addgroup -g 1000 $USERNAME
     adduser -D -H -G $USERNAME -s /bin/false -u 1000 $USERNAME

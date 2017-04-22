4731313eac441ebc92b55000a91d02fa8c56ec15
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-04-22 23:23:18 +0200

    finish first version

--- letsencrypt/run.sh
+++ letsencrypt/run.sh
@@ -2,11 +2,14 @@
 
 set -e
 
-CERT_DIR=/ssl/letsencrypt
+CERT_DIR=/data/letsencrypt
+WORK_DIR=/data/workdir
 CONFIG_PATH=/data/options.json
 
 EMAIL=$(jq --raw-output ".email" $CONFIG_PATH)
 DOMAINS=$(jq --raw-output ".domains[]" $CONFIG_PATH)
+KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
+CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
 
 # setup letsencrypt setup
 if [ ! -f /data/certbot-auto ]; then
@@ -17,16 +20,20 @@ fi
 
 # Start program
 if [ -d $CERT_DIR ]; then
-    ./data/certbot-auto renew -n --config-dir $CERT_DIR
+    /data/certbot-auto renew --non-interactive --config-dir $CERT_DIR --work-dir $WORK_DIR
 else
     # generate domains
-    echo $DOMAINS | while read -r line; do
-        if [ -z DOMAIN_ARG ]; do
+    while IFS=$'\n' read -r line; do
+        if [ -z "$DOMAIN_ARG" ]; then
             DOMAIN_ARG="-d $line"
         else
             DOMAIN_ARG="$DOMAIN_ARG -d $line"
         fi
-    done
+    done <<< "$DOMAINS"
 
-    ./data/certbot-auto certonly -n --standalone --email $EMAIL --config-dir $CERT_DIR $DOMAIN_ARG
+    /data/certbot-auto certonly --non-interactive --standalone --email $EMAIL --config-dir $CERT_DIR --work-dir "$DOMAIN_ARG"
 fi
+
+# copy certs to store
+cp /data/letsencrypt/live/*/privkey.pem /ssl/KEYFILE
+cp /data/letsencrypt/live/*/fullchain.pem /ssl/CERTFILE

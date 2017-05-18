8506bec7841422989bc4fd04fd08f13b22e2f2a1
pvizeli <pvizeli@syshack.ch>
2017-05-18 12:30:02 +0200

    fix domain

--- nginx_proxy/run.sh
+++ nginx_proxy/run.sh
@@ -4,6 +4,7 @@ set -e
 CONFIG_PATH=/data/options.json
 DHPARAMS_PATH=/data/dhparams.pem
 
+DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
 
@@ -17,6 +18,7 @@ fi
 # Prepare config file
 sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
 sed -i "s/%%PRIVKEY%%/$KEYFILE/g" /etc/nginx.conf
+sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
 
 # start server
 echo "[INFO] Run nginx"

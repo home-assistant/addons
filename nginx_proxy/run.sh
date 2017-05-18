bf5e0314d40719809abd9d934b50c4baee4e1988
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-18 13:42:59 +0200

    Nginx proxy (#70)
    Nginx proxy

---
+++ nginx_proxy/run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+DHPARAMS_PATH=/data/dhparams.pem
+
+DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
+KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
+CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
+
+
+# Generate dhparams
+if [ ! -f "$DHPARAMS_PATH" ]; then
+    echo "[INFO] Generate dhparams..."
+    openssl dhparam -dsaparam -out "$DHPARAMS_PATH" 4096 > /dev/null
+fi
+
+# Prepare config file
+sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
+sed -i "s/%%PRIVKEY%%/$KEYFILE/g" /etc/nginx.conf
+sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
+
+# start server
+echo "[INFO] Run nginx"
+exec nginx -c /etc/nginx.conf < /dev/null

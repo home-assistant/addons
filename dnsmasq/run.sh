6237ecb0654f4bcce43679ac03120282a0a68228
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-05-25 00:13:28 +0200

    Add dnsmasq

---
+++ dnsmasq/run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+DEFAULTS=$(jq --raw-output '.domain' $CONFIG_PATH)
+FORWARDS=$(jq --raw-output '.forwards | length' $CONFIG_PATH)
+HOSTS=$(jq --raw-output '.hosts | length' $CONFIG_PATH)
+
+# Add default forward servers
+for line in $DEFAULTS; do
+    echo "server=$line" >> /etc/dnsmasq.conf
+done
+
+# Create domain forwards
+for (( i=0; i < "$FORWARDS"; i++ )); do
+    DOMAIN=$(jq --raw-output ".forwards[$i].domain" $CONFIG_PATH)
+    SERVER=$(jq --raw-output ".forwards[$i].server" $CONFIG_PATH)
+
+    echo "server=/$DOMAIN/$SERVER" >> /etc/dnsmasq.conf
+done
+
+# Create static hosts
+for (( i=0; i < "$HOSTS"; i++ )); do
+    HOST=$(jq --raw-output ".forwards[$i].host" $CONFIG_PATH)
+    IP=$(jq --raw-output ".forwards[$i].ip" $CONFIG_PATH)
+
+    echo "address=/$HOST/$IP" >> /etc/dnsmasq.conf
+done
+
+# run dnsmasq
+exec dnsmasq -C /etc/dnsmasq.conf < /dev/null

50b2ba946c22581aaccb3b7f88b6947c930f71ef
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-06-27 15:12:26 +0200

    Update addons with multibe tasks (#129) (#130)
    Update addons with multibe tasks

--- dnsmasq/run.sh
+++ dnsmasq/run.sh
@@ -6,7 +6,7 @@ CONFIG_PATH=/data/options.json
 DEFAULTS=$(jq --raw-output '.defaults[]' $CONFIG_PATH)
 FORWARDS=$(jq --raw-output '.forwards | length' $CONFIG_PATH)
 HOSTS=$(jq --raw-output '.hosts | length' $CONFIG_PATH)
-INTERFACE=$(jq --raw-output '.interface' $CONFIG_PATH)
+INTERFACE=$(jq --raw-output '.interface // empty' $CONFIG_PATH)
 
 # Bind to interface
 if [ -z "$INTERFACE" ]; then

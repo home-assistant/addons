138fb154770562b5146c2dd7aa72515a7909ac2a
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-06-30 08:20:44 +0200

    Update branche of addons (#137)
    Add an optional extended description…

--- letsencrypt/run.sh
+++ letsencrypt/run.sh
@@ -5,12 +5,20 @@ CERT_DIR=/data/letsencrypt
 WORK_DIR=/data/workdir
 CONFIG_PATH=/data/options.json
 
+CHALLENGE=$(jq --raw-output ".email" $CONFIG_PATH)
 EMAIL=$(jq --raw-output ".email" $CONFIG_PATH)
 DOMAINS=$(jq --raw-output ".domains[]" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
 
 mkdir -p "$CERT_DIR"
+
+# Select challenge
+if [ "$CHALLENGE" == "http" ]; then
+    CERTBOT_CHALLENGE="http-01"
+else
+    CERTBOT_CHALLENGE="tls-sni-01"
+fi
 
 # Generate new certs
 if [ ! -d "$CERT_DIR/live" ]; then
@@ -20,11 +28,11 @@ if [ ! -d "$CERT_DIR/live" ]; then
     done
 
     echo "$DOMAINS" > /data/domains.gen
-    certbot certonly --non-interactive --standalone --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" "${DOMAIN_ARR[@]}"
+    certbot certonly --non-interactive --standalone --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "$CERTBOT_CHALLENGE" "${DOMAIN_ARR[@]}"
 
 # Renew certs
 else
-    certbot renew --non-interactive --config-dir "$CERT_DIR" --work-dir "$WORK_DIR"
+    certbot renew --non-interactive --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "$CERTBOT_CHALLENGE"
 fi
 
 # copy certs to store

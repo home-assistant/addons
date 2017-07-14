d13857db5a00d024aca41e60192e3c43b2c0f584
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-07-14 16:15:53 +0200

    Update run.sh

--- snips/run.sh
+++ snips/run.sh
@@ -3,12 +3,18 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
-MQTT_BRIDGE=$(jq --raw-output '.mqtt_bridge' $CONFIG_PATH)
-MQTT_HOST=$(jq --raw-output '.mqtt_host' $CONFIG_PATH)
-MQTT_PORT=$(jq --raw-output '.mqtt_port' $CONFIG_PATH)
-MQTT_USER=$(jq --raw-output '.mqtt_user' $CONFIG_PATH)
-MQTT_PASSWORD=$(jq --raw-output '.mqtt_password' $CONFIG_PATH)
+MQTT_BRIDGE=$(jq --raw-output '.mqtt_bridge.active' $CONFIG_PATH)
 ASSISTANT=$(jq --raw-output '.assistant' $CONFIG_PATH)
 
+if [ "$MQTT_BRIDG" == "true" ]; then
+  HOST=$(jq --raw-output '.mqtt_bridge.host' $CONFIG_PATH)
+  PORT=$(jq --raw-output '.mqtt_bridge.port' $CONFIG_PATH)
+  USER=$(jq --raw-output '.mqtt_bridge.user' $CONFIG_PATH)
+  PASSWORD=$(jq --raw-output '.mqtt_bridge.password' $CONFIG_PATH)
+fi
 
-exec /opt/snips/snips-entrypoint.sh
+echo "[Info] Start internal mqtt broaker"
+
+
+
+exec /opt/snips/snips-entrypoint.sh --mqtt localhost:1884

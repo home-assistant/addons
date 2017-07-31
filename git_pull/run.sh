83bc1876038a33eb67a46fb798dd8121d2ec6396
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-08-01 00:02:08 +0200

    Add first version of simple git sync

---
+++ git_pull/run.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+REPOSITORIE=$(jq --raw-output '.repositorie' $CONFIG_PATH)
+AUTO_RESTART=$(jq --raw-output '.auto_restart' $CONFIG_PATH)
+REPEAT_ACTIVE=$(jq --raw-output '.repeat.active' $CONFIG_PATH)
+REPEAT_INTERVAL=$(jq --raw-output '.repeat.interval' $CONFIG_PATH)
+
+# init config repositorie
+if [ ! -d /config/.git ];
+    echo "[Info] cleanup config folder and clone from repositorie"
+    rm -rf /config/*
+
+    if ! git clone "$REPOSITORIE" /config 2&> /dev/null; then
+        echo "[Error] can't clone $REPOSITORIE into /config"
+        exit 1
+    fi
+fi
+
+# Run duckdns
+cd /config
+while true; do
+
+    echo "[Info] pull from $REPOSITORIE"
+    git pull 2&> /dev/null || true
+
+    if [ "$AUTO_RESTART" == "true" ];
+        changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+
+        if [! -z "$changed_files" ];
+            echo "[Info] files changed, restart Home-Assistant"
+            curl -s http://172.17.0.2/homeassistant/restart 2&> /dev/null || true
+        fi
+    fi
+
+    # do we repeat?
+    if [ -z "$REPEAT_ACTIVE" ]; then
+        exit 0
+    fi
+    sleep "$REPEAT_INTERVAL"
+done

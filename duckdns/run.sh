a06478cb0c8c729cf7ce30f596f608d3e0490fb0
Dan Spencer <danrspen@gmail.com>
2017-08-08 04:59:04 +0100

    Updated duckdns run script to also update IPv6

--- duckdns/run.sh
+++ duckdns/run.sh
@@ -7,9 +7,12 @@ TOKEN=$(jq --raw-output '.token' $CONFIG_PATH)
 DOMAINS=$(jq --raw-output '.domains | join(",")' $CONFIG_PATH)
 WAIT_TIME=$(jq --raw-output '.seconds' $CONFIG_PATH)
 
+IP_V4=${dig +short myip.opendns.com @resolver1.opendns.com}
+IP_V6=${dig +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com}
+
 # Run duckdns
 while true; do
-    ANSWER="$(curl -sk "https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip=&verbose=true")" || true
+    ANSWER="$(curl -sk "https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&ip=$IP_V4&ipv6=IP_V6&verbose=true")" || true
     echo "$(date): $ANSWER"
     sleep "$WAIT_TIME"
 done

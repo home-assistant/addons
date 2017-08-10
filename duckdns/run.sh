6dc0d707762deead50b9790c1e53733448904602
Dan Spencer <danrspen@gmail.com>
2017-08-10 11:11:29 +0100

    Updated duckdns run.sh to hide timeouts

--- duckdns/run.sh
+++ duckdns/run.sh
@@ -7,8 +7,8 @@ TOKEN=$(jq --raw-output '.token' $CONFIG_PATH)
 DOMAINS=$(jq --raw-output '.domains | join(",")' $CONFIG_PATH)
 WAIT_TIME=$(jq --raw-output '.seconds' $CONFIG_PATH)
 
-IP_V4=${dig +short myip.opendns.com @resolver1.opendns.com}
-IP_V6=${dig +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com}
+IP_V4=$(dig +short myip.opendns.com @resolver1.opendns.com | grep -v "connection timed out")
+IP_V6=$(dig +short -6 myip.opendns.com aaaa @resolver1.ipv6-sandbox.opendns.com | grep -v "connection timed out")
 
 # Run duckdns
 while true; do

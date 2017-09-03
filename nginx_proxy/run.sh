fae7429ef84730d6cc01c10e1ebee13c7b8cdd63
AlexMF <adsmf@users.noreply.github.com>
2017-09-03 15:51:11 +0100

    Change redirection method for incorrect hostname

--- nginx_proxy/run.sh
+++ nginx_proxy/run.sh
@@ -4,15 +4,24 @@ set -e
 CONFIG_PATH=/data/options.json
 DHPARAMS_PATH=/data/dhparams.pem
 
+SNAKEOIL_CERT=/etc/ssl/certs/ssl-cert-snakeoil.pem
+SNAKEOIL_KEY=/etc/ssl/private/ssl-cert-snakeoil.key
+
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
-
 
 # Generate dhparams
 if [ ! -f "$DHPARAMS_PATH" ]; then
     echo "[INFO] Generate dhparams..."
     openssl dhparam -dsaparam -out "$DHPARAMS_PATH" 4096 > /dev/null
+fi
+
+if [ ! -f "$SNAKEOIL_CERT" ]; then
+    echo "[INFO] Create snakeoil (self-signed certificate)"
+    mkdir -p "$(dirname $SNAKEOIL_CERT)"
+    mkdir -p "$(dirname $SNAKEOIL_KEY)"
+    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $SNAKEOIL_KEY -out $SNAKEOIL_CERT -subj '/CN=localhost'
 fi
 
 # Prepare config file

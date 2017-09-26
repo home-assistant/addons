c6ad01656da88d66e59aa1211894e048490685cb
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-09-26 15:19:52 +0200

    Update run.sh

--- duckdns/run.sh
+++ duckdns/run.sh
@@ -5,7 +5,13 @@ CERT_DIR=/data/letsencrypt
 WORK_DIR=/data/workdir
 CONFIG_PATH=/data/options.json
 
-LE_TERMS=$(jq --raw-output ".accept_lets_encrypt_terms" $CONFIG_PATH)
+# Let's encrypt
+LE_TERMS=$(jq --raw-output '.lets_encrypt.accept_terms' $CONFIG_PATH)
+LE_CERTFILE=$(jq --raw-output '.lets_encrypt.certfile' $CONFIG_PATH)
+LE_KEYFILE=$(jq --raw-output '.lets_encrypt.keyfile' $CONFIG_PATH)
+LE_DOMAINS=$(jq --raw-output '.domains[]' $CONFIG_PATH)
+
+# DuckDNS
 TOKEN=$(jq --raw-output '.token' $CONFIG_PATH)
 DOMAINS=$(jq --raw-output '.domains | join(",")' $CONFIG_PATH)
 WAIT_TIME=$(jq --raw-output '.seconds' $CONFIG_PATH)
@@ -13,20 +19,24 @@ WAIT_TIME=$(jq --raw-output '.seconds' $CONFIG_PATH)
 # Register/generate certificate if terms accepted
 if [ "$LE_TERMS" == "true" ]; then
   # Only use first domain for Let's Encrypt
-  DOMAIN=$(jq --raw-output '.domains[0]' $CONFIG_PATH)
+  DOMAIN_ARGS=()
+  for domain in LE_DOMAINS; do
+    DOMAIN_ARGS+=("--domain" "$domain")
+  done
 
   mkdir -p "$CERT_DIR"
   mkdir -p "$WORK_DIR"
 
   # Create empty dehydrated config file so that this dir will be used for storage
-  touch $WORK_DIR/config
+  touch "$WORK_DIR/config"
 
   # Generate new certs
   if [ ! -d "$CERT_DIR/live" ]; then
-    dehydrated --register --accept-terms --config $WORK_DIR/config
+    dehydrated --register --accept-terms --config "$WORK_DIR/config"
   fi
 
-  dehydrated --cron --hook ./hooks.sh --challenge dns-01 --domain $DOMAIN --out $CERT_DIR  --config $WORK_DIR/config
+  dehydrated --cron --hook ./hooks.sh --challenge dns-01 "${DOMAIN_ARGS[@]}" --out "$CERT_DIR"  --config "$WORK_DIR/config"
+  crond
 fi
 
 # Run duckdns

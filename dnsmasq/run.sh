42608115a006e91b2e5a80101633f695929303fa
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-09-26 20:51:21 +0200

    Update run.sh

--- dnsmasq/run.sh
+++ dnsmasq/run.sh
@@ -6,12 +6,6 @@ CONFIG_PATH=/data/options.json
 DEFAULTS=$(jq --raw-output '.defaults[]' $CONFIG_PATH)
 FORWARDS=$(jq --raw-output '.forwards | length' $CONFIG_PATH)
 HOSTS=$(jq --raw-output '.hosts | length' $CONFIG_PATH)
-INTERFACE=$(jq --raw-output '.interface // empty' $CONFIG_PATH)
-
-# Bind to interface
-if [ -z "$INTERFACE" ]; then
-    echo "interface=$INTERFACE" >> /etc/dnsmasq.conf
-fi
 
 # Add default forward servers
 for line in $DEFAULTS; do

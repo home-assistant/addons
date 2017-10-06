cb50a9625724160abad56df42f37c5341267f2d8
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-10-06 17:37:54 +0200

    RPC Shutdown (#184)
    * Create config.json
    
    * Update config.json
    
    * Create Dockerfile
    
    * Create run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update config.json
    
    * Update config.json
    
    * Update config.json

---
+++ rpc_shutdown/run.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+COMPUTERS=$(jq --raw-output '.computers | length' $CONFIG_PATH)
+
+# Read from STDIN aliases to send shutdown
+while read -r input; do
+    echo "[Info] Read alias: $input"
+
+    # Find aliases -> computer
+    for (( i=0; i < "$COMPUTERS"; i++ )); do
+        ALIAS=$(jq --raw-output ".computers[$i].alias" $CONFIG_PATH)
+        ADDRESS=$(jq --raw-output ".computers[$i].address" $CONFIG_PATH)
+        CREDENTIALS=$(jq --raw-output ".computers[$i].credentials" $CONFIG_PATH)
+      
+        # Not the correct alias
+        if [ "$ALIAS" != "$input" ]; then
+            continue
+        fi
+      
+        echo "[Info] Shutdown $input -> $ADDRESS"
+        if ! msg="$(net rpc shutdown -I "$ADDRESS" -U "$CREDENTIALS")"; then
+            echo "[Error] Shutdown fails -> $msg"
+        fi
+    done
+done

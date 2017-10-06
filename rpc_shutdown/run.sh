b72e2103b707699db74e77440b30252dddab3b2a
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-10-06 16:31:32 +0200

    Update run.sh

--- rpc_shutdown/run.sh
+++ rpc_shutdown/run.sh
@@ -6,14 +6,23 @@ CONFIG_PATH=/data/options.json
 COMPUTERS=$(jq --raw-output '.computers | length' $CONFIG_PATH)
 
 # Read from STDIN aliases to send shutdown
-while read -r alias; do
+while read -r input; do
+    echo "[Info] Read alias: $input"
 
-  # Find aliases -> computer
-  for (( i=0; i < "$COMPUTERS"; i++ )); do
-      ALIAS=$(jq --raw-output ".computers[$i].alias" $CONFIG_PATH)
-      ADDRESS=$(jq --raw-output ".computers[$i].address" $CONFIG_PATH)
-      CREDENTIALS=$(jq --raw-output ".computers[$i].credentials" $CONFIG_PATH)
+    # Find aliases -> computer
+    for (( i=0; i < "$COMPUTERS"; i++ )); do
+        ALIAS=$(jq --raw-output ".computers[$i].alias" $CONFIG_PATH)
+        ADDRESS=$(jq --raw-output ".computers[$i].address" $CONFIG_PATH)
+        CREDENTIALS=$(jq --raw-output ".computers[$i].credentials" $CONFIG_PATH)
       
-  done
-
+        # Not the correct alias
+        if [ "$ALIAS" != "$input" ]; then
+            continue
+        fi
+      
+        echo "[Info] Shutdown $input -> $ADDRESS"
+        if ! msg="$(net rpc shutdown -I "$ADDRESS" -U "$CREDENTIALS")"; then
+            echo "[Error] Shutdown fails!\n$msg"
+        fi
+    done
 done

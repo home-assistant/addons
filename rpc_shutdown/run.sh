b242ce4cf51990a3c18eacc4bc43c39a9646d891
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-10-06 16:22:37 +0200

    Update run.sh

--- rpc_shutdown/run.sh
+++ rpc_shutdown/run.sh
@@ -3,7 +3,17 @@ set -e
 
 CONFIG_PATH=/data/options.json
 
+COMPUTERS=$(jq --raw-output '.computers | length' $CONFIG_PATH)
+
 # Read from STDIN aliases to send shutdown
 while read -r alias; do
+
+  # Find aliases -> computer
+  for (( i=0; i < "$COMPUTERS"; i++ )); do
+      ALIAS=$(jq --raw-output ".computers[$i].alias" $CONFIG_PATH)
+      ADDRESS=$(jq --raw-output ".computers[$i].address" $CONFIG_PATH)
+      CREDENTIALS=$(jq --raw-output ".computers[$i].credentials" $CONFIG_PATH)
+      
+  done
 
 done

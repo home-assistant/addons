4481ea7a7679539fe57e190dc003e139e5e66fcb
endor-force <1937941+endor-force@users.noreply.github.com>
2017-11-05 14:12:19 +0100

    Updated build method to match the one in hass.io docker
    Now using bjornes fork of telldus homeassistant-alpine as the source and thus sharing the same source as being used in the homeassistant docker.

--- telldus_core/run.sh
+++ telldus_core/run.sh
@@ -6,8 +6,9 @@ echo Starting run.sh...
 
 if [ ! -f /config/tellstick.conf ]; then
     cp /usr/src/tellstick.conf /config
+	sed -i -e 's/nobody/root/g' /config/tellstick.conf #required until git source is updated otherwise telldus daemon will not communicate with hardware
 fi
 
 telldusd --version 
 #running telldusd with option --nodaemon otherwise the docker stops unexpectedly..
-exec /usr/local/sbin/telldusd --nodaemon
+exec /usr/local/sbin/telldusd --nodaemon < /dev/null

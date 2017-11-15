c29ebb516ab8d4355b22368b6eccfa723f287b8a
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-11-15 14:39:12 +0100

    Update run.sh

--- tellstick/run.sh
+++ tellstick/run.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
-
 set -e
+
+CONFIG_PATH=/data/options.json
+DEVICES=$(jq --raw-output '.forwards | length' $CONFIG_PATH)
 
 echo "[Info] Initialize the tellstick configuration"
 
-# TODO: init config
+# User access
+echo "user = \"root\"" > /etc/tellstick.conf
+
+# devices
+for (for (( i=0; i < "$DEVICES"; i++ )); do
+    DEV_ID = $(jq --raw-output ".DEVICES[$i].id" $CONFIG_PATH)
+    DEV_NAME = $(jq --raw-output ".DEVICES[$i].name" $CONFIG_PATH)
+    DEV_PROTO = $(jq --raw-output ".DEVICES[$i].protocol" $CONFIG_PATH)
+    DEV_MODEL = $(jq --raw-output ".DEVICES[$i].model // emty" $CONFIG_PATH)
+    ATTR_HOUSE = $(jq --raw-output ".DEVICES[$i].house // emty" $CONFIG_PATH)
+    ATTR_CODE = $(jq --raw-output ".DEVICES[$i].code // emty" $CONFIG_PATH)
+    ATTR_UNIT = $(jq --raw-output ".DEVICES[$i].unit // emty" $CONFIG_PATH)
+  
+    (
+        echo ""
+        echo "device {"
+        echo "  id = $DEV_ID"
+        echo "  name = \"$DEV_NAME\""
+        echo "  protocol = \"$DEV_PROTO\""
+        
+        if [! -z "$DEV_MODEL"]; then
+            echo "  model = \"$DEV_MODEL\""
+        fi
+    
+done
+
+echo "[Info] Run telldusd & socat"
 
 # Expose the unix socket to internal network
 socat TCP-LISTEN:50800,reuseaddr,fork UNIX-CONNECT:/tmp/TelldusClient &

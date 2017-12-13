852017be30e7197259025f8f8d6b19ea896e9de3
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-12-13 13:25:26 +0100

    Update run.sh

--- homematic/run.sh
+++ homematic/run.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 set -e
 
+CONFIG_PATH=/data/options.json
+
+TYPE=$(jq --raw-output ".type" $CONFIG_PATH)
+DEVICE=$(jq --raw-output ".device" $CONFIG_PATH)
+
+# Update config
+if [ "$TYPE" == "CCU2" ]; then
+    (
+        echo "Type = CCU2"
+        echo "ComPortFile = $DEVICE"
+        echo "AccessFile = /dev/null"
+        echo "ResetFile = /sys/class/gpio/gpio18/value"
+    ) >> /etc/config/rfd.conf
+fi
+
 $HM_HOME/bin/rfd -d -l 0 -f /opt/hm/etc/config/rfd.conf 
 $HM_HOME/bin/ReGaHss -l 0 -f /opt/hm/etc/rega.conf

7f308c8245f8011bc81c1210bc6c058b17807774
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-12-14 22:36:51 +0100

    Update run.sh

--- homematic/run.sh
+++ homematic/run.sh
@@ -30,12 +30,4 @@ if [ "$(cat /sys/class/gpio/gpio18/direction)" != "out" ]; then
 fi
 
 # Run RFID
-"$HM_HOME/bin/rfd" -c -l 2 -f /opt/hm/etc/config/rfd.conf &
-RFD_PID=$?
-
-# Run Webfrontend
-"$HM_HOME/bin/lighttpd" -f /opt/hm/etc/lighttpd/lighttpd.conf -m /opt/hm/lib/lighttpd/
-"$HM_HOME/bin/ReGaHss" -l 0 -f /opt/hm/etc/rega.conf
-
-# Wait
-wait "$RFD_PID"
+"$HM_HOME/bin/rfd" -c -l 0 -f /opt/hm/etc/config/rfd.conf

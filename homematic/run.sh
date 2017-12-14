9cbc42553cc08976af33ccdbc2135275b8a7ff43
Pascal Vizeli <pascal.vizeli@syshack.ch>
2017-12-14 23:41:29 +0100

    Add-on for OCCU (#209)
    * Create Dockerfile
    
    * Create config.json
    
    * Update config.json
    
    * Update Dockerfile
    
    * Create run.sh
    
    * Create build.json
    
    * Update config.json
    
    * Update config.json
    
    * Update config.json
    
    * Create rfd.conf
    
    * Update Dockerfile
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update Dockerfile
    
    * Update run.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update rfd.conf
    
    * Update run.sh
    
    * Update run.sh
    
    * Update config.json
    
    * Update Dockerfile
    
    * Update run.sh
    
    * Update run.sh
    
    * Update Dockerfile
    
    * Update rfd.conf
    
    * Update run.sh
    
    * Update rfd.conf
    
    * Update config.json
    
    * Update run.sh
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update build.json
    
    * Update run.sh
    
    * Update config.json
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Update config.json
    
    * Update Dockerfile
    
    * Update Dockerfile
    
    * Add files via upload

---
+++ homematic/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+CONFIG_PATH=/data/options.json
+
+TYPE=$(jq --raw-output ".type" $CONFIG_PATH)
+DEVICE=$(jq --raw-output ".device" $CONFIG_PATH)
+
+# Update config
+if [ "$TYPE" == "CCU2" ]; then
+    (
+        echo "Type = CCU2"
+        echo "ComPortFile = $DEVICE"
+        echo "AccessFile = /dev/null"
+        echo "ResetFile = /sys/class/gpio/gpio18/value"
+    ) >> /etc/config/rfd.conf
+fi
+
+# Init folder
+mkdir -p /data/firmware
+
+# Init GPIO
+if [ ! -d /sys/class/gpio/gpio18 ]; then
+    echo 18 > /sys/class/gpio/export
+    sleep 2
+fi
+if [ "$(cat /sys/class/gpio/gpio18/direction)" != "out" ]; then
+    echo out > /sys/class/gpio/gpio18/direction
+    sleep 2
+fi
+
+# Run RFID
+"$HM_HOME/bin/rfd" -c -l 0 -f /opt/hm/etc/config/rfd.conf

8e714890ddbbf14ee05dfa66246c244e6199afe3
Michael Irigoyen <goyney@gmail.com>
2017-12-26 17:55:41 -0500

    Fix linting issues

--- ser2sock/run.sh
+++ ser2sock/run.sh
@@ -7,4 +7,4 @@ SERIAL_DEVICE=$(jq --raw-output ".serialDevice" $CONFIG_PATH)
 BAUD_RATE=$(jq --raw-output ".baudRate" $CONFIG_PATH)
 LISTEN_PORT=$(jq --raw-output ".port" $CONFIG_PATH)
 
-ser2sock -g 2 -f /etc/ser2sock.conf -p ${LISTEN_PORT} -s ${SERIAL_DEVICE} -b ${BAUD_RATE}
+ser2sock -g 2 -f /etc/ser2sock.conf -p "${LISTEN_PORT}" -s "${SERIAL_DEVICE}" -b "${BAUD_RATE}"

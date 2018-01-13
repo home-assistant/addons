44a5e7f8b68eab13e205a534b4fc068367a95e76
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-13 23:44:10 +0100

    fix lint

--- bluetooth_bcm43xx/run.sh
+++ bluetooth_bcm43xx/run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-SERIAL=`grep Serial /proc/cpuinfo | cut -c19-`
+SERIAL="$(grep Serial /proc/cpuinfo | cut -c19-"
 B1=`echo $SERIAL | cut -c3-4`
 B2=`echo $SERIAL | cut -c5-6`
 B3=`echo $SERIAL | cut -c7-8`
-BDADDR=`printf b8:27:eb:%02x:%02x:%02x $((0x$B1 ^ 0xaa)) $((0x$B2 ^ 0xaa)) $((0x$B3 ^ 0xaa))`
+BDADDR="$(printf b8:27:eb:%02x:%02x:%02x $((0x$B1 ^ 0xaa)) $((0x$B2 ^ 0xaa)) $((0x$B3 ^ 0xaa)))"
 
 hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
 hciconfig hci0 up

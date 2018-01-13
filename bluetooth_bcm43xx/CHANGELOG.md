20117849e7302e054ee4dd6e0c96923e626c5fc4
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-13 23:40:02 +0100

    Cleanup bluetooth

--- bluetooth_bcm43xx/CHANGELOG.md
+++ bluetooth_bcm43xx/CHANGELOG.md
@@ -1,5 +1,9 @@
 # Changelog
 
+## 1.1
+- Some cleanups
+- Use new baudrate
+- Set bluetooth mac
+
 ## 1.0
 - Update base image
-- Update Firmware (https://github.com/RPi-Distro/firmware-nonfree)
--- bluetooth_bcm43xx/run.sh
+++ bluetooth_bcm43xx/run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 set -e
 
-hciattach /dev/ttyAMA0 bcm43xx 115200 noflow -
+SERIAL=`grep Serial /proc/cpuinfo | cut -c19-`
+B1=`echo $SERIAL | cut -c3-4`
+B2=`echo $SERIAL | cut -c5-6`
+B3=`echo $SERIAL | cut -c7-8`
+BDADDR=`printf b8:27:eb:%02x:%02x:%02x $((0x$B1 ^ 0xaa)) $((0x$B2 ^ 0xaa)) $((0x$B3 ^ 0xaa))`
+
+hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
 hciconfig hci0 up
 
 while pgrep hciattach > /dev/null

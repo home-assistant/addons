688f43a93df5d3f796b955e5718f52a20f2fafe2
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-13 23:45:31 +0100

    add BD address

--- bluetooth_bcm43xx/run.sh
+++ bluetooth_bcm43xx/run.sh
@@ -7,7 +7,7 @@ B2=`echo $SERIAL | cut -c5-6`
 B3=`echo $SERIAL | cut -c7-8`
 BDADDR="$(printf b8:27:eb:%02x:%02x:%02x $((0x$B1 ^ 0xaa)) $((0x$B2 ^ 0xaa)) $((0x$B3 ^ 0xaa)))"
 
-hciattach /dev/ttyAMA0 bcm43xx 921600 noflow -
+hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - "$BDADDR"
 hciconfig hci0 up
 
 while pgrep hciattach > /dev/null

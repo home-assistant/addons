0beb8718002f04dd18e07b9dfd8eb0baee037955
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-16 14:30:27 +0100

    remove TLS-SMI

--- letsencrypt/run.sh
+++ letsencrypt/run.sh
@@ -5,20 +5,12 @@ CERT_DIR=/data/letsencrypt
 WORK_DIR=/data/workdir
 CONFIG_PATH=/data/options.json
 
-CHALLENGE=$(jq --raw-output ".challenge" $CONFIG_PATH)
 EMAIL=$(jq --raw-output ".email" $CONFIG_PATH)
 DOMAINS=$(jq --raw-output ".domains[]" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
 
 mkdir -p "$CERT_DIR"
-
-# Select challenge
-if [ "$CHALLENGE" == "http" ]; then
-    CERTBOT_CHALLENGE="http"
-else
-    CERTBOT_CHALLENGE="tls-sni"
-fi
 
 # Generate new certs
 if [ ! -d "$CERT_DIR/live" ]; then
@@ -28,11 +20,11 @@ if [ ! -d "$CERT_DIR/live" ]; then
     done
 
     echo "$DOMAINS" > /data/domains.gen
-    certbot certonly --non-interactive --standalone --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "$CERTBOT_CHALLENGE" "${DOMAIN_ARR[@]}"
+    certbot certonly --non-interactive --standalone --email "$EMAIL" --agree-tos --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "http" "${DOMAIN_ARR[@]}"
 
 # Renew certs
 else
-    certbot renew --non-interactive --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "$CERTBOT_CHALLENGE"
+    certbot renew --non-interactive --config-dir "$CERT_DIR" --work-dir "$WORK_DIR" --preferred-challenges "http"
 fi
 
 # copy certs to store

5a74d6e84eb2fbb561b09f871d9b90e703498997
Maarten Groeneweg <maarten@groeneweg.eu>
2018-01-21 10:06:01 +0100

    moved npm install into dockerfile

--- hue_mqtt_bridge/run.sh
+++ hue_mqtt_bridge/run.sh
@@ -6,16 +6,8 @@ CONFIG_PATH=/data/options.json
 INSTALL_PATH=/bin/hue-mqtt-bridge
 ################
 
-
-## generate config file ##
-echo "[Info] starting shell script"
-mkdir "$INSTALL_PATH"
-git clone https://github.com/dale3h/hue-mqtt-bridge.git "$INSTALL_PATH"
-cd "$INSTALL_PATH"
-npm i 
-
-## copy config file
+echo "[Info] copy setting to config file"
 cp "$CONFIG_PATH" "$INSTALL_PATH/config.json"
 
-## run module ##
-./bin/hue-mqtt-bridge
+echo "[Info] starting Hue MQTT Bridge"
+node /bin/hue-mqtt-bridge/index.js

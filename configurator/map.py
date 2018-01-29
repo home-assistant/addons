3db6974bee926adc5002ca17d7c3c74796a14d34
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-01-29 10:00:52 +0100

    Update map.py

--- configurator/map.py
+++ configurator/map.py
@@ -13,7 +13,7 @@ with hassio_options.open('r') as json_file:
 configurator = {
     'BASEPATH': "/config",
     'HASS_API': "http://hassio/homeassistant/api/",
-    'HASS_API_PASSWORD': os.environ.get('API_TOKEN'),
+    'HASS_API_PASSWORD': os.environ.get('HASSIO_TOKEN', ''),
     'CREDENTIALS':
         "{}:{}".format(options['username'], options['password']),
     'SSL_CERTIFICATE':

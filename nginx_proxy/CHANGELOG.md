e5078a79290febd480b41ed42ed1a270183d6857
Ville Skyttä <ville.skytta@iki.fi>
2018-03-10 01:46:12 +0200

    nginx_proxy: Include /ssl/nginx_proxy/<domain>*.conf in nginx.conf (#259)
    * nginx_proxy: Allow customization with included config files from /share
    
    Provides a way to add additional configuration to the default, for
    example proxy_pass locations before the default root location, as well
    as additional complete server configs.
    
    * nginx_proxy: Run logo.png through zopflipng -m
    
    * nginx_proxy: Bump version to 1.0, add CHANGELOG.md
    
    * Update CHANGELOG.md

---
+++ nginx_proxy/CHANGELOG.md
@@ -0,0 +1,6 @@
+# Changelog
+
+## 1.0
+- Add customization mechanism using included config snippets from /share
+- Optimize logo.png
+- Update base image
--- nginx_proxy/run.sh
+++ nginx_proxy/run.sh
@@ -10,6 +10,7 @@ SNAKEOIL_KEY=/data/ssl-cert-snakeoil.key
 DOMAIN=$(jq --raw-output ".domain" $CONFIG_PATH)
 KEYFILE=$(jq --raw-output ".keyfile" $CONFIG_PATH)
 CERTFILE=$(jq --raw-output ".certfile" $CONFIG_PATH)
+CUSTOMIZE_ACTIVE=$(jq --raw-output ".customize.active" $CONFIG_PATH)
 
 # Generate dhparams
 if [ ! -f "$DHPARAMS_PATH" ]; then
@@ -26,6 +27,14 @@ fi
 sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
 sed -i "s/%%PRIVKEY%%/$KEYFILE/g" /etc/nginx.conf
 sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
+
+# Allow customize configs from share
+if [ "$CUSTOMIZE_ACTIVE" == "true" ]; then
+    CUSTOMIZE_DEFAULT=$(jq --raw-output ".customize.default" $CONFIG_PATH)
+    sed -i "s|#include /share/nginx_proxy_default.*|include /share/$CUSTOMIZE_DEFAULT;|" /etc/nginx.conf
+    CUSTOMIZE_SERVERS=$(jq --raw-output ".customize.servers" $CONFIG_PATH)
+    sed -i "s|#include /share/nginx_proxy/.*|include /share/$CUSTOMIZE_SERVERS;|" /etc/nginx.conf
+fi
 
 # start server
 echo "[INFO] Run nginx"

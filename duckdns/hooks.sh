1a0d5e85ae61f241ff5ffbf661cb56df4558da0e
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-03-26 21:32:19 +0200

    Fix hook warning (#272)
    * Fix hook warning
    
    * Update config.json
    
    * Update CHANGELOG.md

--- duckdns/hooks.sh
+++ duckdns/hooks.sh
@@ -8,6 +8,7 @@ TOKEN=$(jq --raw-output '.token' $CONFIG_PATH)
 CERTFILE=$(jq --raw-output '.lets_encrypt.certfile' $CONFIG_PATH)
 KEYFILE=$(jq --raw-output '.lets_encrypt.keyfile' $CONFIG_PATH)
 
+# https://github.com/lukas2511/dehydrated/blob/master/docs/examples/hook.sh
 case "$1" in
     "deploy_challenge")
         curl -s "https://www.duckdns.org/update?domains=$DOMAINS&token=$TOKEN&txt=$4"
@@ -18,15 +19,5 @@ case "$1" in
     "deploy_cert")
         cp -f "$5" "/ssl/$CERTFILE"
         cp -f "$3" "/ssl/$KEYFILE"
-        ;;
-    "unchanged_cert")
-        ;;
-    "startup_hook")
-        ;;
-    "exit_hook")
-        ;;
-    *)
-        echo Unknown hook "${1}"
-        exit 0
         ;;
 esac

f1f322b4ee05552a0ec07255837c1415e1a34f58
tschmidty69 <tschmidty@yahoo.com>
2018-04-24 19:47:17 -0400

    added customtts and default assistants

--- snips/run.sh
+++ snips/run.sh
@@ -5,12 +5,58 @@ CONFIG_PATH=/data/options.json
 
 MQTT_BRIDGE=$(jq --raw-output '.mqtt_bridge.active' $CONFIG_PATH)
 ASSISTANT=$(jq --raw-output '.assistant' $CONFIG_PATH)
+SPEAKER=$(jq --raw-output '.speaker' $CONFIG_PATH)
+MIC=$(jq --raw-output '.mic' $CONFIG_PATH)
+LANG=$(jq --raw-output '.language' $CONFIG_PATH| awk -F '-' '{print $1}')
+CUSTOMTTS=$(jq --raw-output '.custom_tts' $CONFIG_PATH)
+PLATFORM=$(jq --raw-output '.tts_platform' $CONFIG_PATH)
+API_KEY=$(jq --raw-output '.api_key' // apikey $CONFIG_PATH)
+
+if [ "$HOSTTYPE" == "x86_64" ]; then
+    ARCH="amd64"
+else
+    ARCH="armhf"
+fi
+echo "[INFO] ARCH: $ARCH"
+
+echo "[INFO] Show audio output devices"
+aplay -l
+
+echo "[INFO] Show audio input devices"
+arecord -l
+
+echo "[INFO] Setup audio device"
+if [ -f "/share/asoundrc" ]; then
+    echo "[INFO] - Installing /share/asoundrc"
+    cp -v /share/asoundrc /root/.asoundrc
+else
+    echo "[INFO] - Using default asound.conf"
+    sed -i "s/%%SPEAKER%%/$SPEAKER/g" /root/.asoundrc
+    sed -i "s/%%MIC%%/$MIC/g" /root/.asoundrc
+fi
+
+echo "[DEBUG] Using /root/.asoundrc"
+cat /root/.asoundrc
 
 echo "[INFO] Checking for /share/snips.toml"
 if [ -f "/share/snips.toml" ]; then
-    echo "[INFO] Installing /share/snips.toml"
+    echo "[INFO] - Installing /share/snips.toml"
     cp -v /share/snips.toml /etc/
 fi
+
+if [ "$CUSTOMTTS" == "true" ]; then
+    if [ -z "$PLATFORM" ]; then
+        echo "[ERROR] - platform must be set to use custom tts!"
+    else
+        echo "[INFO] - Using custom tts"
+        echo "customtts = { command = [\"/usr/bin/customtts.sh\", \"$API_KEY\" \"$PLATFORM\", \"%%OUTPUT_FILE%%\", \"$LANG\", \"%%TEXT%%\"] }" >> /etc/snips.toml
+    fi
+else
+    echo "[INFO] - Using default tts (picotts)"
+fi
+
+echo "[INFO] Copying mpg123 for $ARCH"
+mv -v /usr/bin/mpg123-$ARCH /usr/bin/mpg123
 
 # mqtt bridge
 if [ "$MQTT_BRIDGE" == "true" ]; then
@@ -40,6 +86,8 @@ if [ "$MQTT_BRIDGE" == "true" ]; then
         echo "topic hermes/asr/stopListening out"
         echo "topic hermes/asr/startListening out"
         echo "topic hermes/nlu/intentNotParsed out"
+        echo "topic hermes/audioServer/+/playBytes/# out"
+        echo "topic hermes/audioServer/+/playFinished out"
         echo "topic # IN hermes/"
     } >> /etc/mosquitto.conf
 fi
@@ -53,10 +101,14 @@ echo "[INFO] Checking for updated $ASSISTANT in /share"
 if [ -f "/share/$ASSISTANT" ]; then
     echo "[INFO] Install/Update snips assistant"
     unzip -o -u "/share/$ASSISTANT" -d /usr/share/snips
-# otherwise use the default
-elif [ -f "/assistant-default.zip" ]; then
-    echo "[INFO] Using default snips assistant"
-    unzip -o -u "/assistant-default.zip" -d /usr/share/snips
+# otherwise use the default 
+else
+    if [ -f "/share/assistant-Hass-$LANG.zip" ]; then
+        echo "[INFO] - Using default assistant-Hass-$LANG.zip"
+        unzip -o -u "/shareassistant-Hass-$LANG.zip" -d /usr/share/snips
+    else
+        echo "[ERROR] Could not find assistant!"
+    fi
 fi
 
 echo "[INFO] Starting snips-watch"

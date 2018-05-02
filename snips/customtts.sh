f9088167be46a0e8f0e99c4b172727a90399369f
tschmidty69 <tschmidty@yahoo.com>
2018-05-02 13:50:24 -0400

    Added some quotes

--- snips/customtts.sh
+++ snips/customtts.sh
@@ -8,7 +8,7 @@ LANG=$3
 TEXT=$4
 
 MESSAGE="\"{\\\"message\\\": \\\"$TEXT\\\", \\\"platform\\\": \\\"$PLATFORM\\\"}\""
-echo $MESSAGE
+echo "$MESSAGE"
 
 RESPONSE=$(eval curl -s -H \"x-ha-access: $API_KEY\" -H \"Type: application/json\" http://hassio/homeassistant/api/tts_get_url -d $MESSAGE)
 if [ "$RESPONSE" = "" ]; then
@@ -16,7 +16,7 @@ if [ "$RESPONSE" = "" ]; then
 fi
 echo $RESPONSE
 
-URL=`echo $RESPONSE | jq --raw-output '.url'`
+URL=$(echo "$RESPONSE" | jq --raw-output '.url')
 if [ "$URL" = "" ]; then
     exit 1
 fi
@@ -24,7 +24,7 @@ fi
 rm /tmp/temp.mp3
 curl -s -H "x-ha-access: $API_KEY" "$URL" -o /tmp/temp.mp3
 if [ -f /tmp/temp.mp3 ]; then
-  /usr/bin/mpg123 -w $FILE /tmp/temp.mp3
+  /usr/bin/mpg123 -w "$FILE" /tmp/temp.mp3
 fi
 rm /tmp/temp.mp3
 

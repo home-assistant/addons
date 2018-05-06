f7f349d7f758db3c9120f9993a58b8de95575784
tschmidty69 <tschmidty@yahoo.com>
2018-05-06 16:38:51 -0400

    updated version

--- snips/run.sh
+++ snips/run.sh
@@ -7,7 +7,7 @@ MQTT_BRIDGE=$(jq --raw-output '.mqtt_bridge.active' $CONFIG_PATH)
 ASSISTANT=$(jq --raw-output '.assistant' $CONFIG_PATH)
 LANG=$(jq --raw-output '.language' $CONFIG_PATH| awk -F '-' '{print $1}')
 CUSTOMTTS=$(jq --raw-output '.custom_tts["active"]' $CONFIG_PATH)
-PLATFORM=$(jq --raw-output '.custom_tts["platform"].' $CONFIG_PATH)
+PLATFORM=$(jq --raw-output '.custom_tts["platform"]' $CONFIG_PATH)
 
 echo "[INFO] LANG: $LANG"
 

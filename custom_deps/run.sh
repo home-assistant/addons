05753245f7d9695fed7854a2ad1985838ebd1dfd
Mories <m.kok@mories.com>
2018-07-04 19:01:54 +0200

    fixed for loop

--- custom_deps/run.sh
+++ custom_deps/run.sh
@@ -6,8 +6,9 @@ CONFIG_PATH=/data/options.json
 PYPI=$(jq --raw-output ".pypi[]" $CONFIG_PATH)
 APK=$(jq --raw-output ".apk[] // empty" $CONFIG_PATH)
 
-# create bash array from comma seperated  
-IFS=',' read -r -a packages <<< $(jq --raw-output ".pypi[]" $CONFIG_PATH)
+# create bash array from comma seperated value using Internal field seperator
+IFS=','
+read -ra packages <<< $PYPI
 
 # Cleanup old deps
 echo "[Info] Remove old deps"
@@ -25,10 +26,10 @@ fi
 # Install pypi modules
 echo "[Info] Install pypi modules into deps"
 export PYTHONUSERBASE=/config/deps
-for package in $packages
+for package in ${packages[@]}
 do
   echo "About to install $package"
-  if ! ERROR="$(pip3 install --user --no-cache-dir --prefix= --no-dependencies $package)"; then
+  if ! ERROR="$(pip3 install --user --no-cache-dir --prefix= --no-dependencies "$package")"; then
       echo "[Error] Can't install pypi packages!"
       echo "$ERROR" && exit 1
   fi

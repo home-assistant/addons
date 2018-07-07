fadb7f237af06bfbd975d413705d336481b77b97
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-07-08 00:38:02 +0200

    Fix bluetooth

--- bluetooth_bcm43xx/run.sh
+++ bluetooth_bcm43xx/run.sh
@@ -4,10 +4,8 @@ set -e
 # Read serial number
 if [ -e /device-tree/serial-number ]; then
     SERIAL="$(cut -c9- /device-tree/serial-number)"
-    BAUD=115200
 else
     SERIAL="$(grep Serial /proc/cpuinfo | cut -c19-)"
-    BAUD=921600
 fi
 
 # Generate MAC
@@ -16,8 +14,11 @@ B2="$(echo "$SERIAL" | cut -c5-6)"
 B3="$(echo "$SERIAL" | cut -c7-8)"
 BDADDR="$(printf b8:27:eb:%02x:%02x:%02x $((0x$B1 ^ 0xaa)) $((0x$B2 ^ 0xaa)) $((0x$B3 ^ 0xaa)))"
 
-hciattach /dev/ttyAMA0 bcm43xx 921600 "$BAUD" - "$BDADDR"
+hciattach /dev/ttyAMA0 bcm43xx 921600 - "$BDADDR"
+sleep 1
+
 hciconfig hci0 up
+hciconfig hci0 piscan
 
 while pgrep hciattach > /dev/null
 do

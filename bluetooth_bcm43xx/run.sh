a8136223023437db0e071db69e245e47238c1fee
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-07-08 01:23:36 +0200

    Change baud of rpi (#344)
    * Update run.sh
    
    * Fix bluetooth
    
    * Update CHANGELOG.md
    
    * Update config.json

--- bluetooth_bcm43xx/run.sh
+++ bluetooth_bcm43xx/run.sh
@@ -14,8 +14,11 @@ B2="$(echo "$SERIAL" | cut -c5-6)"
 B3="$(echo "$SERIAL" | cut -c7-8)"
 BDADDR="$(printf b8:27:eb:%02x:%02x:%02x $((0x$B1 ^ 0xaa)) $((0x$B2 ^ 0xaa)) $((0x$B3 ^ 0xaa)))"
 
-hciattach /dev/ttyAMA0 bcm43xx 921600 noflow - "$BDADDR"
+hciattach /dev/ttyAMA0 bcm43xx 921600 - "$BDADDR"
+sleep 1
+
 hciconfig hci0 up
+hciconfig hci0 piscan
 
 while pgrep hciattach > /dev/null
 do

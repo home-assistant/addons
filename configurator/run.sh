07f7634e94b9735ed64170baf559237cc3e81a24
Daniel Perna <danielperna84@gmail.com>
2018-08-03 23:05:45 +0200

    Update to Configurator 0.3.1 (#357)
    * Update to configurator 0.3.1
    
    * Fix tests
    
    * Forgot SSL
    
    * Update config.json
    
    * Update run.sh
    
    * Update run.sh
    
    * Update run.sh
    
    * Update config.json
    
    * Update run.sh
    
    * Removed verify_hostname option
    
    * Update config.json
    
    * Update run.sh
    
    * Update build.json
    
    * Update Dockerfile
    
    * Update Dockerfile

--- configurator/run.sh
+++ configurator/run.sh
@@ -1,8 +1,46 @@
 #!/bin/bash
-set -e
+# shellcheck disable=SC2155
 
-# Map hassio value into hass-configurator options
-python3 /map.py /tmp/configurator.json
+CONFIG_PATH=/data/options.json
+
+export HC_BASEPATH=/config
+export HC_HASS_API=http://hassio/homeassistant/api/
+export HC_HASS_API_PASSWORD=$HASSIO_TOKEN
+export HC_GIT=true
+export HC_VERIFY_HOSTNAME=false
+export HC_IGNORE_SSL=false
+
+export HC_ENFORCE_BASEPATH=$(jq --raw-output '.enforce_basepath' $CONFIG_PATH)
+export HC_USERNAME=$(jq --raw-output '.username' $CONFIG_PATH)
+export HC_PASSWORD=$(jq --raw-output '.password' $CONFIG_PATH)
+export HC_NOTIFY_SERVICE=$(jq --raw-output '.notify_service' $CONFIG_PATH)
+export HC_ALLOWED_NETWORKS=$(jq --raw-output '.allowed_networks | join(",")' $CONFIG_PATH)
+export HC_BANNED_IPS=$(jq --raw-output '.banned_ips | join(",")' $CONFIG_PATH)
+export HC_BANLIMIT=$(jq --raw-output '.banlimit' $CONFIG_PATH)
+export HC_IGNORE_PATTERN=$(jq --raw-output '.ignore_pattern | join(",")' $CONFIG_PATH)
+export HC_DIRSFIRST=$(jq --raw-output '.dirsfirst' $CONFIG_PATH)
+export HC_VERIFY_HOSTNAME=$(jq --raw-output '.verify_hostname // empty' $CONFIG_PATH)
+
+SSL=$(jq --raw-output '.ssl // false' $CONFIG_PATH)
+if  [ "$SSL" == "true" ]; then
+    export HC_SSL_CERTIFICATE=$(jq --raw-output '.certfile' $CONFIG_PATH)
+    export HC_SSL_KEY=$(jq --raw-output '.keyfile' $CONFIG_PATH)
+fi
+
+LOGLEVEL=$(jq --raw-output '.loglevel // empty' $CONFIG_PATH)
+if [ ! -z "$LOGLEVEL" ]; then
+    export HC_LOGLEVEL=$LOGLEVEL
+fi
+
+SESAME=$(jq --raw-output '.sesame // empty' $CONFIG_PATH)
+if [ ! -z "$SESAME" ]; then
+    export HC_SESAME=$SESAME
+fi
+
+SESAME_TOTP_SECRET=$(jq --raw-output '.sesame_totp_secret // empty' $CONFIG_PATH)
+if [ ! -z "$SESAME_TOTP_SECRET" ]; then
+    export HC_SESAME_TOTP_SECRET=$SESAME_TOTP_SECRET
+fi
 
 # Run configurator
-exec python3 /configurator.py /tmp/configurator.json
+exec python3 /configurator.py

fe7c83f5038f21f02f920e12fb398666b7636889
Pascal Vizeli <pvizeli@syshack.ch>
2018-11-05 12:38:06 +0100

    Update run.sh

--- snips/run.sh
+++ snips/run.sh
@@ -34,10 +34,10 @@ fi
 
 # Use Hass.io mqtt services
 if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/services/mqtt)"; then
-    HOST=$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.host')
-    PORT=$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.port')
-    USER=$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')
-    PASSWORD=$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')
+    HOST="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.host')"
+    PORT="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.port')"
+    USER="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')"
+    PASSWORD="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')"
 
     echo "[INFO] Setup Hass.io mqtt service to ${HOST}"
 
@@ -66,6 +66,7 @@ if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/se
     ) >> /etc/mosquitto.conf
 else
     echo "[WARN] No Hass.io mqtt service found!"
+    exit 1
 fi
 
 echo "[INFO] Start internal mqtt broker"

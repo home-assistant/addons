0aca513e6bb80a9ba9e6a91a059a161fc24e1f80
Pascal Vizeli <pvizeli@syshack.ch>
2018-11-05 21:15:34 +0100

    Update customtts.sh

--- snips/customtts.sh
+++ snips/customtts.sh
@@ -11,19 +11,19 @@ MESSAGE="\"{\\\"message\\\": \\\"${TEXT}\\\", \\\"platform\\\": \\\"${PLATFORM}\
 echo "${MESSAGE}"
 
 RESPONSE=$(eval curl -s  -d "${MESSAGE}" -H \"Authorization: Bearer "${API_KEY}"\" -H \"Type: application/json\" http://hassio/homeassistant/api/tts_get_url)
-if [ -z "${RESPONSE}"]; then
+if [ -z "${RESPONSE}" ]; then
     exit 1
 fi
 echo "${RESPONSE}"
 
 URL="$(echo "${RESPONSE}" | jq --raw-output '.url')"
-if [ -z "$URL"]; then
+if [ -z "${URL}" ]; then
     exit 1
 fi
 
-rm /tmpfs/temp.mp3
+rm -f /tmpfs/temp.mp3
 curl -s -H "Authorization: Bearer ${API_KEY}" "${URL}" -o /tmpfs/temp.mp3
 if [ -f /tmpfs/temp.mp3 ]; then
   /usr/bin/mpg123 -w "${FILE}" /tmpfs/temp.mp3
 fi
-rm /tmpfs/temp.mp3
+rm -f /tmpfs/temp.mp3

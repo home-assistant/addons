ced589ca90ed82b21fc48941be97783772589962
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-11-09 12:58:48 +0100

    Update run.sh

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -5,7 +5,7 @@ set -e
 echo "[Info] Install TensorFlow modules into deps"
 export PYTHONUSERBASE=/config/deps
 
-PYPI="absl-py==0.1.6 astor==0.6.0 gast==0.2.0 keras_applications==1.0.6 keras_preprocessing==1.0.5"
+PYPI="absl-py==0.1.6 astor==0.6.0 gast==0.2.0 keras_applications==1.0.6 keras_preprocessing==1.0.5 h5py==2.8.0"
 
 # shellcheck disable=SC2086
 if ! pip3 install --user --no-cache-dir --prefix= --no-dependencies ${PYPI}; then

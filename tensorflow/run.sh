bf5912356ed48f0cb446d2a7cf4712fdb478284c
Pascal Vizeli <pascal.vizeli@syshack.ch>
2018-11-09 12:18:22 +0100

    Update run.sh

--- tensorflow/run.sh
+++ tensorflow/run.sh
@@ -5,6 +5,8 @@ set -e
 echo "[Info] Install TensorFlow modules into deps"
 export PYTHONUSERBASE=/config/deps
 
+PYPI="absl-py==0.1.6 astor==0.6.0 gast==0.2.0 keras_applications==1.0.6 keras_preprocessing==1.0.5"
+
 # shellcheck disable=SC2086
 if ! pip3 install --user --no-cache-dir --prefix= --no-dependencies ${PYPI}; then
     echo "[Error] Can't install PyPI packages!"
@@ -14,5 +16,7 @@ fi
 echo "[Info] Install TensorFlow into deps"
 if ! pip3 install --user --no-cache-dir --prefix= --no-dependencies /usr/src/tensorflow_pkg/tensorflow-$TENSORFLOW_VERSION-cp36-cp36m-linux_*.whl; then
     echo "[Error] Can't install TensorFlow package!"
+    exit 1
 fi
 
+echo "[INFO] TensorFlow installed and ready for use"

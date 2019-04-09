1bbe315d3e9ce802956721dd9de6b36bd068ba81
Pascal Vizeli <pascal.vizeli@syshack.ch>
2019-04-09 15:39:24 +0200

    Configurator 3.1 (#563)
    * Add options to configurator
    
    * set chmod
    
    * Fix style
    
    * Update version

--- configurator/data/run.sh
+++ configurator/data/run.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bashio
+set -e
 
+DIRSFIRST=$(bashio::config 'dirsfirst')
+ENFORCE_BASEPATH=$(bashio::config 'enforce_basepath')
 WAIT_PIDS=()
 
 # Setup and run Frontend
 sed -i "s/%%PORT%%/8080/g" /etc/nginx/nginx-ingress.conf
 sed -i "s/%%PORT_INGRESS%%/8099/g" /etc/nginx/nginx-ingress.conf
+
 nginx -c /etc/nginx/nginx-ingress.conf &
 WAIT_PIDS+=($!)
 
 # Setup and run configurator
 sed -i "s/%%TOKEN%%/${HASSIO_TOKEN}/g" /etc/configurator.conf
+sed -i "s/%%DIRSFIRST%%/${DIRSFIRST}/g" /etc/configurator.conf
+sed -i "s/%%ENFORCE_BASEPATH%%/${ENFORCE_BASEPATH}/g" /etc/configurator.conf
+
 hass-configurator /etc/configurator.conf &
 WAIT_PIDS+=($!)
 

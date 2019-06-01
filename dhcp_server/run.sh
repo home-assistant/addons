0db9407c293047a51ed21ff7f57040d91a7c0197
Franck Nijhof <frenck@addons.community>
2019-06-01 18:04:08 +0200

    dhcp_server: Rewrite add-on onto Bashio

--- dhcp_server/run.sh
+++ dhcp_server/run.sh
@@ -1,59 +1,69 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 set -e
 
-CONFIG_PATH=/data/options.json
+CONFIG="/etc/dhcpd.conf"
+LEASES="/data/dhcpd.lease"
 
-DEFAULT_LEASE=$(jq --raw-output '.default_lease' $CONFIG_PATH)
-MAX_LEASE=$(jq --raw-output '.max_lease' $CONFIG_PATH)
-DOMAIN=$(jq --raw-output '.domain' $CONFIG_PATH)
-DNS=$(jq --raw-output '.dns | join(", ")' $CONFIG_PATH)
-NETWORKS=$(jq --raw-output '.networks | length' $CONFIG_PATH)
-HOSTS=$(jq --raw-output '.hosts | length' $CONFIG_PATH)
+bashio::log.info "Creating DHCP configuration..."
 
-sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/dhcpd.conf
-sed -i "s/%%DNS_SERVERS%%/$DNS/g" /etc/dhcpd.conf
-sed -i "s/%%DEFAULT_LEASE%%/$DEFAULT_LEASE/g" /etc/dhcpd.conf
-sed -i "s/%%MAX_LEASE%%/$MAX_LEASE/g" /etc/dhcpd.conf
+# Create main config
+DEFAULT_LEASE=$(bashio::config 'default_lease')
+DNS=$(bashio::config 'dns|join(", ")')
+DOMAIN=$(bashio::config 'domain')
+MAX_LEASE=$(bashio::config 'max_lease')
+
+{
+    echo "option domain-name ${DOMAIN};"
+    echo "option domain-name-servers ${DNS};";
+    echo "default-lease-time ${DEFAULT_LEASE};"
+    echo "max-lease-time ${MAX_LEASE};"
+    echo "authoritative;"
+} > "${CONFIG}"
 
 # Create networks
-for (( i=0; i < "$NETWORKS"; i++ )); do
-    SUBNET=$(jq --raw-output ".networks[$i].subnet" $CONFIG_PATH)
-    NETMASK=$(jq --raw-output ".networks[$i].netmask" $CONFIG_PATH)
-    RANGE_START=$(jq --raw-output ".networks[$i].range_start" $CONFIG_PATH)
-    RANGE_END=$(jq --raw-output ".networks[$i].range_end" $CONFIG_PATH)
-    BROADCAST=$(jq --raw-output ".networks[$i].broadcast" $CONFIG_PATH)
-    GATEWAY=$(jq --raw-output ".networks[$i].gateway" $CONFIG_PATH)
-    INTERFACE=$(jq --raw-output ".networks[$i].interface" $CONFIG_PATH)
+for network in $(bashio::config 'networks|keys'); do
+    BROADCAST=$(bashio::config "networks[${network}].broadcast")
+    GATEWAY=$(bashio::config "networks[${network}].gateway")
+    INTERFACE=$(bashio::config "networks[${network}].interface")
+    NETMASK=$(bashio::config "networks[${network}].netmask")
+    RANGE_END=$(bashio::config "networks[${network}].range_end")
+    RANGE_START=$(bashio::config "networks[${network}].range_start")
+    SUBNET=$(bashio::config "networks[${network}].subnet")
 
     {
-        echo "subnet $SUBNET netmask $NETMASK {"
-        echo "  range $RANGE_START $RANGE_END;"
-        echo "  interface $INTERFACE;"
-        echo "  option routers $GATEWAY;"
-        echo "  option broadcast-address $BROADCAST;"
+        echo "subnet ${SUBNET} netmask ${NETMASK} {"
+        echo "  interface ${INTERFACE};"
+        echo "  range ${RANGE_START} ${RANGE_END};"
+        echo "  option routers ${GATEWAY};"
+        echo "  option broadcast-address ${BROADCAST};"
         echo "}"
-    } >> /etc/dhcpd.conf
+    } >> "${CONFIG}"
 done
 
 # Create hosts
-for (( i=0; i < "$HOSTS"; i++ )); do
-    MAC=$(jq --raw-output ".hosts[$i].mac" $CONFIG_PATH)
-    NAME=$(jq --raw-output ".hosts[$i].name" $CONFIG_PATH)
-    IP=$(jq --raw-output ".hosts[$i].ip" $CONFIG_PATH)
+for host in $(bashio::config 'hosts|keys'); do
+    IP=$(bashio::config "hosts[${host}].ip")
+    MAC=$(bashio::config "hosts[${host}].mac")
+    NAME=$(bashio::config "hosts[${host}].name")
 
     {
-        echo "host $NAME {"
-        echo "  hardware ethernet $MAC;"
-        echo "  fixed-address $IP;"
-        echo "  option host-name \"$NAME\";"
+        echo "host ${NAME} {"
+        echo "  hardware ethernet ${MAC};"
+        echo "  fixed-address ${IP};"
+        echo "  option host-name \"${NAME}\";"
         echo "}"
-    } >> /etc/dhcpd.conf
+    } >> "${CONFIG}"
 done
 
 # Create database
-if [ ! -f /data/dhcpd.lease ]; then
-    touch /data/dhcpd.lease
+if ! bashio::fs.file_exists "${LEASES}"; then
+    touch "${LEASES}"
 fi
 
-# run dhcp server
-exec /usr/sbin/dhcpd -4 -f -d --no-pid -lf /data/dhcpd.lease -cf /etc/dhcpd.conf < /dev/null
+# Start DHCP server
+bashio::log.info "Starting DHCP server..."
+exec /usr/sbin/dhcpd \
+    -4 -f -d --no-pid \
+    -lf "${LEASES}" \
+    -cf "${CONFIG}" \
+    < /dev/null

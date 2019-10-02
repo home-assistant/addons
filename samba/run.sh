9375da8650c2e095029307dfbd4445fdbbd01232
Zapfmeister <zapfmeister@gmail.com>
2019-10-02 10:12:27 +0200

    Update samba add-on to use Bashio #735

--- samba/run.sh
+++ samba/run.sh
@@ -1,29 +1,29 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 set -e
 
 CONFIG_PATH=/data/options.json
 
-WORKGROUP=$(jq --raw-output '.workgroup' $CONFIG_PATH)
-INTERFACE=$(jq --raw-output '.interface // empty' $CONFIG_PATH)
-ALLOW_HOSTS=$(jq --raw-output '.allow_hosts | join(" ")' $CONFIG_PATH)
-USERNAME=$(jq --raw-output '.username // empty' $CONFIG_PATH)
-PASSWORD=$(jq --raw-output '.password // empty' $CONFIG_PATH)
+WORKGROUP=$(bashio::config 'workgroup')
+INTERFACE=$(bashio::config 'interface')
+ALLOW_HOSTS=$(bashio::config 'allow_hosts')
+USERNAME=$(bashio::config 'username')
+PASSWORD=$(bashio::config 'password')
 
 WAIT_PIDS=()
 NAME=
 
 # Check Login data
 if [ -z "${USERNAME}" ] || [ -z "${PASSWORD}" ]; then
-    echo "[ERROR] No valid login data inside options!"
+    bashio::log.error "No valid login data inside options!"
     exit 1
 fi
 
 # Read hostname from API
 if ! NAME="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/info | jq --raw-output '.data.hostname')"; then
-    echo "[WARN] Can't read hostname, use default!"
+    bashio::log.warn "Can't read hostname, use default!"
     NAME="hassio"
 else
-    echo "[INFO] Read hostname: ${NAME}"
+    bashio::log.info "Read hostname: ${NAME}"
 fi
 
 # Setup config
@@ -32,6 +32,8 @@ sed -i "s|%%NAME%%|$NAME|g" /etc/smb.conf
 sed -i "s|%%INTERFACE%%|$INTERFACE|g" /etc/smb.conf
 sed -i "s|%%ALLOW_HOSTS%%|$ALLOW_HOSTS|g" /etc/smb.conf
 sed -i "s|%%USERNAME%%|$USERNAME|g" /etc/smb.conf
+
+echo "Allowed Hosts: " $ALLOW_HOSTS
 
 # Init users
 addgroup "${USERNAME}"
@@ -48,10 +50,10 @@ WAIT_PIDS+=($!)
 
 # Register stop
 function stop_samba() {
-    echo "Kill Processes..."
+    bashio::log.info "Kill Processes..."
     kill -15 "${WAIT_PIDS[@]}"
     wait "${WAIT_PIDS[@]}"
-    echo "Done."
+    bashio::log.info "Done."
 }
 trap "stop_samba" SIGTERM SIGHUP
 

f4fee7f316fd649dad1d8ef740eb6bb8d9bdecb8
Moritz Fey <moritz.fey@gmail.com>
2019-10-08 19:15:08 +0200

    use bashio where possible

--- snips/run.sh
+++ snips/run.sh
@@ -1,35 +1,34 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 set -e
 
-CONFIG_PATH=/data/options.json
-
-ASSISTANT=$(jq --raw-output '.assistant' $CONFIG_PATH)
-LANG=$(jq --raw-output '.language // "en"' $CONFIG_PATH)
-CUSTOMTTS=$(jq --raw-output '.custom_tts.active' $CONFIG_PATH)
+ASSISTANT=$(bashio::config 'assistant')
+LANG=$(bashio::config 'language')
+CUSTOMTTS=$(bashio::config 'custom_tts.active')
 MQTT_CONFIG=
 
-echo "[INFO] LANG: ${LANG}"
 
-echo "[INFO] Checking for /share/snips.toml"
+bashio::log.info "LANG: ${LANG}"
+bashio::log.info "Checking for /share/snips.toml"
+
 if [ -f "/share/snips.toml" ]; then
-    echo "[INFO] - Installing /share/snips.toml"
+    bashio::log.info "Installing /share/snips.toml"
     cp -v /share/snips.toml /etc/
 fi
 
-if [ "${CUSTOMTTS}" == "true" ]; then
-    PLATFORM=$(jq --raw-output '.custom_tts.platform' $CONFIG_PATH)
+if [ bashio::config.true 'custom_tts.active' ]; then
+    PLATFORM=$(bashio::config 'custom_tts.platform')
 
     if [ -z "${PLATFORM}" ]; then
-        echo "[ERROR] - platform must be set to use custom tts!"
+        bashio::log.error "platform must be set to use custom tts!"
     else
-        echo "[INFO] - Using custom tts"
+        bashio::log.info "Using custom tts"
         (
-            echo "provider = \"customtts\""
-            echo "customtts = { command = [\"/usr/bin/customtts.sh\", \"${PLATFORM}\", \"%%OUTPUT_FILE%%\", \"${LANG}\", \"%%TEXT%%\"] }"
+            bashio::log.info "provider = \"customtts\""
+            bashio::log.info "customtts = { command = [\"/usr/bin/customtts.sh\", \"${PLATFORM}\", \"%%OUTPUT_FILE%%\", \"${LANG}\", \"%%TEXT%%\"] }"
         ) >> /etc/snips.toml
     fi
 else
-    echo "[INFO] - Using default tts (picotts)"
+    bashio::log.info "Using default tts (picotts)"
 fi
 
 # Use Hass.io mqtt services
@@ -39,59 +38,59 @@ if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/se
     USER="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')"
     PASSWORD="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')"
 
-    echo "[INFO] Setup Hass.io mqtt service to ${HOST}"
+    bashio::log.info "Setup Hass.io mqtt service to ${HOST}"
 
     (
-        echo "connection main-mqtt"
-        echo "address ${HOST}:${PORT}"
+        bashio::log.info "connection main-mqtt"
+        bashio::log.info "address ${HOST}:${PORT}"
     ) >> /etc/mosquitto.conf
 
     if [ -n "${USER}" ]; then
       (
-          echo "username ${USER}"
-          echo "password ${PASSWORD}"
+          bashio::log.info "username ${USER}"
+          bashio::log.info "password ${PASSWORD}"
       ) >> /etc/mosquitto.conf
     fi
 
     (
-        echo "topic hermes/intent/# out"
-        echo "topic hermes/hotword/toggleOn out"
-        echo "topic hermes/hotword/toggleOff out"
-        echo "topic hermes/hotword/+/detected out"
-        echo "topic hermes/asr/stopListening out"
-        echo "topic hermes/asr/startListening out"
-        echo "topic hermes/nlu/intentNotParsed out"
-        echo "topic hermes/audioServer/+/playBytes/# out"
-        echo "topic hermes/audioServer/+/playFinished out"
-        echo "topic # IN hermes/"
+        bashio::log.info "topic hermes/intent/# out"
+        bashio::log.info "topic hermes/hotword/toggleOn out"
+        bashio::log.info "topic hermes/hotword/toggleOff out"
+        bashio::log.info "topic hermes/hotword/+/detected out"
+        bashio::log.info "topic hermes/asr/stopListening out"
+        bashio::log.info "topic hermes/asr/startListening out"
+        bashio::log.info "topic hermes/nlu/intentNotParsed out"
+        bashio::log.info "topic hermes/audioServer/+/playBytes/# out"
+        bashio::log.info "topic hermes/audioServer/+/playFinished out"
+        bashio::log.info "topic # IN hermes/"
     ) >> /etc/mosquitto.conf
 else
-    echo "[ERROR] No Hass.io mqtt service found!"
+    bashio::log.error "No Hass.io mqtt service found!"
 fi
 
-echo "[INFO] Start internal mqtt broker"
+bashio::log.info "Start internal mqtt broker"
 mosquitto -c /etc/mosquitto.conf &
 
 
-echo "[INFO] Checking for updated ${ASSISTANT} in /share"
+bashio::log.info "Checking for updated ${ASSISTANT} in /share"
 # check if a new assistant file exists
 if [ -f "/share/${ASSISTANT}" ]; then
-    echo "[INFO] Install/Update snips assistant"
+    bashio::log.info "Install/Update snips assistant"
     rm -rf /usr/share/snips/assistant
     unzip -o -u "/share/${ASSISTANT}" -d /usr/share/snips
 # otherwise use the default
 else
-    echo "[INFO] Checking for /assistant_Hass_${LANG}.zip"
+    bashio::log.info "Checking for /assistant_Hass_${LANG}.zip"
     if [ -f "/assistant_Hass_${LANG}.zip" ]; then
-        echo "[INFO] - Using default assistant_Hass_${LANG}.zip"
+        bashio::log.info "Using default assistant_Hass_${LANG}.zip"
         rm -rf /usr/share/snips/assistant
         unzip -o -u "/assistant_Hass_${LANG}.zip" -d /usr/share/snips
     else
-        echo "[ERROR] Could not find assistant!"
+        bashio::log.error "Could not find assistant!"
     fi
 fi
 
-echo "[INFO] Starting snips-watch"
+bashio::log.info "Starting snips-watch"
 ( sleep 2; /usr/bin/snips-watch -vvv --no_color ) &
 
 /usr/bin/snips-asr --version

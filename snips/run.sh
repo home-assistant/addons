140cb59c6bdd79b5d5d0e050cf8f689cf538757d
Moritz Fey <moritz.fey@gmail.com>
2019-10-09 21:32:20 +0200

    use bashio in run.sh

--- snips/run.sh
+++ snips/run.sh
@@ -1,35 +1,32 @@
-#!/bin/bash
+#!/usr/bin/env bashio
 set -e
 
-CONFIG_PATH=/data/options.json
+ASSISTANT=$(bashio::config 'assistant')
+LANG=$(bashio::config 'language')
+CUSTOMTTS=$(bashio::config 'custom_tts.active')
 
-ASSISTANT=$(jq --raw-output '.assistant' $CONFIG_PATH)
-LANG=$(jq --raw-output '.language // "en"' $CONFIG_PATH)
-CUSTOMTTS=$(jq --raw-output '.custom_tts.active' $CONFIG_PATH)
-MQTT_CONFIG=
+bashio::log.info "LANG: ${LANG}"
+bashio::log.info "Checking for /share/snips.toml"
 
-echo "[INFO] LANG: ${LANG}"
-
-echo "[INFO] Checking for /share/snips.toml"
-if [ -f "/share/snips.toml" ]; then
-    echo "[INFO] - Installing /share/snips.toml"
+if bashio::fs.file_exists "/share/snips.toml"; then
+    bashio::log.info "Installing /share/snips.toml"
     cp -v /share/snips.toml /etc/
 fi
 
-if [ "${CUSTOMTTS}" == "true" ]; then
-    PLATFORM=$(jq --raw-output '.custom_tts.platform' $CONFIG_PATH)
+if bashio::config.true 'custom_tts.active'; then
+    PLATFORM=$(bashio::config 'custom_tts.platform')
 
-    if [ -z "${PLATFORM}" ]; then
-        echo "[ERROR] - platform must be set to use custom tts!"
+    if ! bashio::config.has_value 'custom_tts.platform'; then
+        bashio::exit.nok "platform must be set to use custom tts!"
     else
-        echo "[INFO] - Using custom tts"
+        bashio::log.info "Using custom tts"
         (
             echo "provider = \"customtts\""
             echo "customtts = { command = [\"/usr/bin/customtts.sh\", \"${PLATFORM}\", \"%%OUTPUT_FILE%%\", \"${LANG}\", \"%%TEXT%%\"] }"
         ) >> /etc/snips.toml
     fi
 else
-    echo "[INFO] - Using default tts (picotts)"
+    bashio::log.info "Using default tts (picotts)"
 fi
 
 # Use Hass.io mqtt services
@@ -39,14 +36,14 @@ if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/se
     USER="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.username')"
     PASSWORD="$(echo "${MQTT_CONFIG}" | jq --raw-output '.data.password')"
 
-    echo "[INFO] Setup Hass.io mqtt service to ${HOST}"
+    bashio::log.info "Setup Hass.io mqtt service to ${HOST}"
 
     (
         echo "connection main-mqtt"
         echo "address ${HOST}:${PORT}"
     ) >> /etc/mosquitto.conf
 
-    if [ -n "${USER}" ]; then
+    if bashio::var.defined USER; then
       (
           echo "username ${USER}"
           echo "password ${PASSWORD}"
@@ -66,32 +63,32 @@ if MQTT_CONFIG="$(curl -s -f -H "X-Hassio-Key: ${HASSIO_TOKEN}" http://hassio/se
         echo "topic # IN hermes/"
     ) >> /etc/mosquitto.conf
 else
-    echo "[ERROR] No Hass.io mqtt service found!"
+    bashio::log.error "No Hass.io mqtt service found!"
 fi
 
-echo "[INFO] Start internal mqtt broker"
+bashio::log.info "Start internal mqtt broker"
 mosquitto -c /etc/mosquitto.conf &
 
 
-echo "[INFO] Checking for updated ${ASSISTANT} in /share"
+bashio::log.info "Checking for updated ${ASSISTANT} in /share"
 # check if a new assistant file exists
-if [ -f "/share/${ASSISTANT}" ]; then
-    echo "[INFO] Install/Update snips assistant"
+if bashio::fs.file_exists "/share/${ASSISTANT}"; then
+    bashio::log.info "Install/Update snips assistant"
     rm -rf /usr/share/snips/assistant
     unzip -o -u "/share/${ASSISTANT}" -d /usr/share/snips
 # otherwise use the default
 else
-    echo "[INFO] Checking for /assistant_Hass_${LANG}.zip"
-    if [ -f "/assistant_Hass_${LANG}.zip" ]; then
-        echo "[INFO] - Using default assistant_Hass_${LANG}.zip"
+    bashio::log.info "Checking for /assistant_Hass_${LANG}.zip"
+    if bashio::fs.file_exists "/assistant_Hass_${LANG}.zip"; then
+        bashio::log.info "Using default assistant_Hass_${LANG}.zip"
         rm -rf /usr/share/snips/assistant
         unzip -o -u "/assistant_Hass_${LANG}.zip" -d /usr/share/snips
     else
-        echo "[ERROR] Could not find assistant!"
+        bashio::exit.nok "Could not find assistant!"
     fi
 fi
 
-echo "[INFO] Starting snips-watch"
+bashio::log.info "Starting snips-watch"
 ( sleep 2; /usr/bin/snips-watch -vvv --no_color ) &
 
 /usr/bin/snips-asr --version

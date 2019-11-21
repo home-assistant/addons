0e78528b6f2c5cf03b9a6e450b839b949cea65cf
Pascal Vizeli <pvizeli@syshack.ch>
2019-11-21 13:50:32 +0100

    Update ADA 0.7 (#848)

--- ada/data/run.sh
+++ ada/data/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bashio
 
+STT=$(bashio::config 'stt')
+TTS=$(bashio::config 'tts')
 
-exec python3 -m ada
+exec python3 -m ada --url "http://hassio/homeassistant" --key "$HASSIO_TOKEN" --stt "$STT" --tts "$TTS"

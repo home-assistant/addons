eed27504b8c9ddca810070250eb827900904ae18
Franck Nijhof <git@frenck.dev>
2019-12-18 20:27:08 +0100

    google_assistant: Convert add-on to Bashio

--- google_assistant/data/run.sh
+++ google_assistant/data/run.sh
@@ -1,27 +1,26 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bashio
 
-CONFIG_PATH=/data/options.json
 CLIENT_JSON=/data/client.json
 CRED_JSON=/data/cred.json
 
-CLIENT_SECRETS=$(jq --raw-output '.client_secrets' $CONFIG_PATH)
-PROJECT_ID=$(jq --raw-output '.project_id' $CONFIG_PATH)
-MODEL_ID=$(jq --raw-output '.model_id' $CONFIG_PATH)
+CLIENT_SECRETS=$(bashio::config 'client_secrets')
+MODEL_ID=$(bashio::config 'model_id')
+PROJECT_ID=$(bashio::config 'project_id')
 
 # check if a new assistant file exists
-if [ -f "/share/$CLIENT_SECRETS" ]; then
-    echo "[Info] Install/Update service client_secrets file"
-    cp -f "/share/$CLIENT_SECRETS" "$CLIENT_JSON"
+if bashio::fs.file_exists "/share/${CLIENT_SECRETS}"; then
+    bashio::log.info "Installing/Updating service client_secrets file"
+    cp -f "/share/${CLIENT_SECRETS}" "${CLIENT_JSON}"
 fi
 
-if [ ! -f "$CRED_JSON" ] && [ -f "$CLIENT_JSON" ]; then
-    echo "[Info] Start WebUI for handling oauth2"
-    python3 /hassio_oauth.py "$CLIENT_JSON" "$CRED_JSON"
-elif [ ! -f "$CRED_JSON" ]; then
-    echo "[Error] You need initialize GoogleAssistant with a client secret json!"
-    exit 1
+if ! bashio::fs.file_exists "${CRED_JSON}" && bashio::fs.file_exists "${CLIENT_JSON}";
+then
+    bashio::log.info "Starting WebUI for handling OAuth2..."
+    python3 /hassio_oauth.py "${CLIENT_JSON}" "${CRED_JSON}"
+elif ! bashio::fs.file_exists "${CRED_JSON}"; then
+    bashio::exit.nok "You need initialize Google Assistant with a client secret JSON!"
 fi
 
-echo "[Info] Run Hass.io GAssisant SDK"
-exec python3 /hassio_gassistant.py "$CRED_JSON" "$PROJECT_ID" "$MODEL_ID" < /dev/null
+basio::log.info "Starting Hass.io GAssisant SDK..."
+exec python3 /hassio_gassistant.py \
+    "${CRED_JSON}" "${PROJECT_ID}" "${MODEL_ID}" < /dev/null

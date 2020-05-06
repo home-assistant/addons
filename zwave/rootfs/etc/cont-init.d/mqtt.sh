f9b001e1da2f0d3ff6d17b8eb681cd5d86fd0ee6
Pascal Vizeli <pvizeli@syshack.ch>
2020-05-06 15:47:16 +0200

    Add support for new openzwave

---
+++ zwave/rootfs/etc/cont-init.d/mqtt.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Setup mqtt settings
+# ==============================================================================
+
+if ! bashio::services.available "mqtt"; then
+    bashio::log.info "No internal MqTT service found"
+    bashio::exit.ok
+else
+    host=$(bashio::services "mqtt" "host")
+    password=$(bashio::services "mqtt" "password")
+    port=$(bashio::services "mqtt" "port")
+    username=$(bashio::services "mqtt" "username")
+
+    (
+        echo "connection main-mqtt"
+        echo "address ${host}:${port}"
+        echo "username ${username}"
+        echo "password ${password}"
+
+        echo "topic OpenZWave/# out"
+        echo "topic # IN OpenZWave/"
+    ) >> /etc/mosquitto.conf
+fi

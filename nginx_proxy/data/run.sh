3b842cfbe127f57116eabe692d2b11e87561e0c6
Troy Kelly <troy@troykelly.com>
2020-05-12 08:52:38 +1000

    Slashes break file paths for certificates

--- nginx_proxy/data/run.sh
+++ nginx_proxy/data/run.sh
@@ -49,9 +49,9 @@ if bashio::config.true 'cloudflare'; then
 fi
 
 # Prepare config file
-sed -i "s/%%FULLCHAIN%%/$CERTFILE/g" /etc/nginx.conf
-sed -i "s/%%PRIVKEY%%/$KEYFILE/g" /etc/nginx.conf
-sed -i "s/%%DOMAIN%%/$DOMAIN/g" /etc/nginx.conf
+sed -i "s|%%FULLCHAIN%%|$CERTFILE|g" /etc/nginx.conf
+sed -i "s|%%PRIVKEY%%|$KEYFILE|g" /etc/nginx.conf
+sed -i "s|%%DOMAIN%%|$DOMAIN|g" /etc/nginx.conf
 
 [ -n "$HSTS" ] && HSTS="add_header Strict-Transport-Security \"$HSTS\" always;"
 sed -i "s/%%HSTS%%/$HSTS/g" /etc/nginx.conf

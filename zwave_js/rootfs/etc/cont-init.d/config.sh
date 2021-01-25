85ed6f62b4d4317307c79b090edaa845ed6385e1
Martin Hjelmare <marhje52@gmail.com>
2021-01-25 11:00:44 +0100

    Fix and clean zwave_js add-on

--- zwave_js/rootfs/etc/cont-init.d/config.sh
+++ zwave_js/rootfs/etc/cont-init.d/config.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Generate Zwave JS config file
+# Generate Z-Wave JS config file
 # ==============================================================================
 
 # Generate config
 tempio \
     -conf /data/options.json \
-    -template /usr/share/tempio/zwave_config.conf \
-    -out /etc/zwave_config.conf
+    -template /usr/share/tempio/zwave_config.json \
+    -out /etc/zwave_config.json

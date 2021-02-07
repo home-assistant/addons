db5ca171f21c609924ddc25d2941fcf83255f5f4
Oscar Calvo <oscar@calvonet.com>
2021-02-07 14:04:02 -0600

    Support srv-host records

--- dnsmasq/data/run.sh
+++ dnsmasq/data/run.sh
@@ -26,6 +26,17 @@ for host in $(bashio::config 'hosts|keys'); do
     echo "address=/${HOST}/${IP}" >> "${CONFIG}"
 done
 
+# Create static srv-hosts
+for srvhost in $(bashio::config 'srv-hosts|keys'); do
+    SRV=$(bashio::config "srv-hosts[${srvhost}].srv")
+    HOST=$(bashio::config "srv-hosts[${srvhost}].host")
+    PORT=$(bashio::config "srv-hosts[${srvhost}].port")
+    PRIORITY=$(bashio::config "srv-hosts[${srvhost}].priority")
+    WEIGHT=$(bashio::config "srv-hosts[${srvhost}].weight")
+
+    echo "srv-host=${SRV},${HOST},${PORT},${PRIORITY},${WEIGHT}" >> "${CONFIG}"
+done
+
 # Run dnsmasq
 bashio::log.info "Starting dnsmasq..."
 exec dnsmasq -C "${CONFIG}" -z < /dev/null

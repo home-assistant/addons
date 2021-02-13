c2f49160faa98e903424482ea8dfb957f715337f
Oscar Calvo <oscar@calvonet.com>
2021-02-13 17:42:46 -0600

    Refactor run and config

--- dnsmasq/rootfs/etc/cont-init.d/config.sh
+++ dnsmasq/rootfs/etc/cont-init.d/config.sh
@@ -1,20 +1,12 @@
-#!/usr/bin/bash
+#!/usr/bin/with-contenv bashio
 # ==============================================================================
-# DNSMASQ setup
+# DNSMASQ config
 # ==============================================================================
-
-set -e
 
 CONFIG="/etc/dnsmasq.conf"
-
 bashio::log.info "Configuring dnsmasq..."
-
 tempio \
     -conf /data/options.json \
     -template /usr/share/tempio/dnsmasq.config \
     -out "${CONFIG}"
-
-# Run dnsmasq
-bashio::log.info "Starting dnsmasq..."
-exec dnsmasq -C "${CONFIG}" -z < /dev/null
 

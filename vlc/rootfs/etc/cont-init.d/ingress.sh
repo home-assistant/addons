9fd88b5e3535e5ca84ab60e734e61876e2550b6e
Pascal Vizeli <pvizeli@syshack.ch>
2021-02-16 17:49:06 +0100

    Add VLC add-on (#1862)
    * Add VLC add-on
    
    * remove armhf support

---
+++ vlc/rootfs/etc/cont-init.d/ingress.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Prepair VLC for ingress access
+# ==============================================================================
+
+# Generate NGINX config
+bashio::var.json \
+    supervisor_ip "$(getent hosts supervisor | awk '{ print $1 }' | head -1)" \
+    | tempio \
+        -template /usr/share/tempio/nginx.conf \
+        -out /etc/nginx/nginx.conf

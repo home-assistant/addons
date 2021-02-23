04045c0ffa0fc854fe4437a21cd8cb5ee755e7db
Joel Moses <joelmoses2000@gmail.com>
2021-02-22 21:33:13 -0800

    Use the generated secret for the Ingress credential for nginx

--- vlc/rootfs/etc/cont-init.d/ingress.sh
+++ vlc/rootfs/etc/cont-init.d/ingress.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
+# Create VLC secret
+# ==============================================================================
+
+if bashio::fs.file_exists /data/secret; then
+    bashio::exit.ok
+else
+    pwgen 64 1 > /data/secret
+fi
+
+# Generate password
+
+
+
+# ==============================================================================
 # Prepair VLC for ingress access
 # ==============================================================================
+
 
 # Generate NGINX config
 bashio::var.json \
     supervisor_ip "$(getent hosts supervisor | awk '{ print $1 }' | head -1)" \
+    ingress_secret "$(echo :`cat /data/secret` | base64)" \
     | tempio \
         -template /usr/share/tempio/nginx.conf \
         -out /etc/nginx/nginx.conf

cdec4f8d38d87a0834b1634ffef4c248a3e9e1e6
Pascal Vizeli <pvizeli@syshack.ch>
2021-06-18 10:45:22 +0000

    Store deconz persistent

--- deconz/rootfs/etc/cont-init.d/deconz.sh
+++ deconz/rootfs/etc/cont-init.d/deconz.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Manage deCONZ firmware
+# Manage deCONZ firmware/folder
 # ==============================================================================
 
 bashio::log.info "$(/usr/bin/GCFFlasher_internal -l)"
+
+mkdir -p /root/.local/share
+mkdir -p /data/dresden-elektronik
+ln -s /data/dresden-elektronik /root/.local/share/dresden-elektronik

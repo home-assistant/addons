b6d5ec3cf683aa58ac2388e3d9d2eefc431730d7
Jens Maus <mail@jens-maus.de>
2022-01-20 10:03:32 +0100

    minor cleanup change.

--- homematic/rootfs/opt/hm/bin/createBackup.sh
+++ homematic/rootfs/opt/hm/bin/createBackup.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # shellcheck shell=dash disable=SC2169 source=/dev/null
 #
-# simple wrapper script to generate a CCU compatible sbk file
+# simple backup script to create a CCU compatible .sbk type
+# backup out of the core-homematic HomeAssistant add-on
 #
-# Copyright (c) 2016-2021 Jens Maus <mail@jens-maus.de>
+# Copyright (c) 2022 Jens Maus <mail@jens-maus.de>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -65,9 +66,18 @@ if [[ -d "${TMPDIR}" ]]; then
   rm -f "${TMPDIR}/usr/local/etc/config/groups.gson" "${TMPDIR}/usr/local/etc/config/userprofiles"
   cp -a /data/* "${TMPDIR}/usr/local/etc/config/"
 
+  # cleanup
+  rm -f "${TMPDIR}/usr/local/etc/config/options.json"
+  rm -f "${TMPDIR}/usr/local/etc/config/groups.json"
+  rm -f "${TMPDIR}/usr/local/etc/config/HMServer.conf"
+  rm -f "${TMPDIR}/usr/local/etc/config/hmip_user.conf"
+  rm -f "${TMPDIR}/usr/local/etc/config/log4j.xml"
+  rm -f "${TMPDIR}/usr/local/etc/config/rega.conf"
+  rm -f "${TMPDIR}/usr/local/etc/config/rega_user.conf"
+
   # create a gzipped tar of /usr/local
   set +e # disable abort on error
-  /bin/tar -C "${TMPDIR}" --owner=root --group=root --exclude=usr/local/tmp --exclude=usr/local/lost+found --exclude="${BACKUPDIR}" --exclude-tag=.nobackup --one-file-system --ignore-failed-read --warning=no-file-changed -czf "${TMPDIR}/usr_local.tar.gz" usr/local 2>/dev/null
+  /bin/tar -C "${TMPDIR}" --owner=root --group=root --exclude=usr/local/tmp --exclude=usr/local/lost+found --exclude="${BACKUPDIR}" --exclude-tag=.nobackup --ignore-failed-read --warning=no-file-changed -czf "${TMPDIR}/usr_local.tar.gz" usr/local 2>/dev/null
   if [[ $? -eq 2 ]]; then
     exit 2
   fi

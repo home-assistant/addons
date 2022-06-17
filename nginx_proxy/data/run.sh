9527c60b56abcbe8704275fcc37987fe61a3f746
pyos <pyos100500@gmail.com>
2022-06-18 00:43:21 +0200

    nginx_proxy: reject SSL handshake with incorrect domain
    This is slightly faster (terminates after client hello) and removes
    the need for a self-signed certificate.

--- nginx_proxy/data/run.sh
+++ nginx_proxy/data/run.sh
@@ -3,9 +3,6 @@ set -e
 
 DHPARAMS_PATH=/data/dhparams.pem
 
-SNAKEOIL_CERT=/data/ssl-cert-snakeoil.pem
-SNAKEOIL_KEY=/data/ssl-cert-snakeoil.key
-
 CLOUDFLARE_CONF=/data/cloudflare.conf
 
 DOMAIN=$(bashio::config 'domain')
@@ -19,11 +16,6 @@ HA_PORT=$(bashio::core.port)
 if ! bashio::fs.file_exists "${DHPARAMS_PATH}"; then
     bashio::log.info  "Generating dhparams (this will take some time)..."
     openssl dhparam -dsaparam -out "$DHPARAMS_PATH" 4096 > /dev/null
-fi
-
-if ! bashio::fs.file_exists "${SNAKEOIL_CERT}"; then
-    bashio::log.info "Creating 'snakeoil' self-signed certificate..."
-    openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout $SNAKEOIL_KEY -out $SNAKEOIL_CERT -subj '/CN=localhost'
 fi
 
 if bashio::config.true 'cloudflare'; then

721bf52a833f949cc5d003cdd6c8e51fffddf76a
d-grund <48119748+d-grund@users.noreply.github.com>
2023-05-26 23:14:04 +0200

    Delete mosquitto directory

--- mosquitto/rootfs/etc/cont-init.d/nginx.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Configures NGINX
-# ==============================================================================
-# This template only uses environment vars, no input
-echo "{}" \
-  | tempio \
-    -template /usr/share/tempio/nginx.gtpl \
-    -out /etc/nginx/nginx.conf

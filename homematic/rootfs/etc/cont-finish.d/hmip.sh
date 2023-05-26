a2d6958bd84739e6e288f5edece339e3935e2270
d-grund <48119748+d-grund@users.noreply.github.com>
2023-05-26 23:12:26 +0200

    Delete homematic directory

--- homematic/rootfs/etc/cont-finish.d/hmip.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Backup hmip_address
-# ==============================================================================
-if bashio::config.true 'hmip_enable' && [ -f /data/hmip_address.conf ]; then
-    cp -f /etc/config/hmip_address.conf /data/ || bashio::log.warning "Failed to backup hmip address"
-fi

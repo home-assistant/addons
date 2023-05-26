d70fa17b6d4bf7c9bf983728ee9478c4c7ffabc5
d-grund <48119748+d-grund@users.noreply.github.com>
2023-05-26 23:20:46 +0200

    Delete silabs-multiprotocol directory

--- silabs-multiprotocol/rootfs/etc/cont-init.d/check-cpcd-shm.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Check that no other CPC instance is running on this system
-# ==============================================================================
-if test -d /dev/shm/cpcd; then
-    bashio::log.error "Another CPC daemon running!"
-    exit 1
-fi
--- silabs-multiprotocol/rootfs/etc/s6-overlay/scripts/enable-check.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/etc/s6-overlay/scripts/otbr-enable-check.sh
-/etc/s6-overlay/scripts/socat-cpcd-tcp-enable-check.sh

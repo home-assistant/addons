221db11887be164bedcee1b3cb5aa578ab4dff53
Franck Nijhof <git@frenck.dev>
2024-01-23 10:42:02 +0100

    Clean up HomeMatic CCU add-on

--- homematic/rootfs/etc/cont-init.d/nginx.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Configure NGINX for use with ReGaHss
-# ==============================================================================
-ingress_entry=$(bashio::addon.ingress_entry)
-sed -i "s#%%ingress_entry%%#${ingress_entry}#g" /etc/nginx/nginx.conf

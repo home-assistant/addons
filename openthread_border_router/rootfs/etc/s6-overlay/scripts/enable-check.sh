440eda9f90c33808f34db2e386b8578247be2d19
Tim Lunn <tl@smlight.tech>
2024-04-03 23:26:11 +1100

    squash enable checks into enable-check hook script

--- openthread_border_router/rootfs/etc/s6-overlay/scripts/enable-check.sh
+++ openthread_border_router/rootfs/etc/s6-overlay/scripts/enable-check.sh
@@ -1,2 +1,22 @@
-/etc/s6-overlay/scripts/otbr-web-enable-check.sh
-/etc/s6-overlay/scripts/socat-otbr-tcp-enable-check.sh
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Disable OTBR Web if necessary ports are not exposed
+# ==============================================================================
+
+if bashio::var.has_value "$(bashio::addon.port 8080)" \
+     && bashio::var.has_value "$(bashio::addon.port 8081)"; then
+    bashio::log.info "Web UI and REST API port are exposed, starting otbr-web."
+else
+    rm /etc/s6-overlay/s6-rc.d/user/contents.d/otbr-web
+    bashio::log.info "The otbr-web is disabled."
+fi
+
+# ==============================================================================
+# Enable socat-otbr-tcp service if needed
+# ==============================================================================
+
+if bashio::config.has_value 'network_device'; then
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/socat-otbr-tcp
+    touch /etc/s6-overlay/s6-rc.d/otbr-agent/dependencies.d/socat-otbr-tcp
+    bashio::log.info "Enabled socat-otbr-tcp."
+fi

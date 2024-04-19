2bb1632b0005504f95ec7cfe8642209e861ab04a
Csongor Zalatnai <csongor@zalatnai.hu>
2024-04-19 17:21:24 +0200

    silabs-multiprotocol: Fixed BLE config key name #2

--- silabs-multiprotocol/rootfs/etc/s6-overlay/scripts/bt-hci-enable-check.sh
+++ silabs-multiprotocol/rootfs/etc/s6-overlay/scripts/bt-hci-enable-check.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/with-contenv bashio
 # ==============================================================================
-# Enable socat-cpcd-tcp service if needed
+# Enable bt-hci service if needed
 # ==============================================================================
 
-if bashio::config.has_value 'bt_hci_enable'; then
+if bashio::config.true 'bt_hci_enable'; then
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/bt-hci
     bashio::log.info "Enabled bt-hci"
 fi

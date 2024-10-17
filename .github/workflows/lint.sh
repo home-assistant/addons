0fb3343f691b2a9aa7db90b10e4d15dd093e1341
Mislav Marohnić <git@mislav.net>
2024-10-17 19:49:20 +0200

    Lint all script files in the repository
    The "lint" workflow that runs for all pull requests in this repository
    used the `ludeeus/action-shellcheck@2.0.0` action as a shellcheck
    wrapper. However, that wrapper only lints files with known extensions
    and known shebangs, leading to lots of of scripts in this repository not
    being subject to linting even though they are technically bash scripts.
    
    Furthermore, there was no visibility into this linting problem, because
    the linting step neither printed the list of files processed nor was
    there a way to run the linter script locally.
    
    This change:
    
    1. Replaces the usage of the `ludeeus/action-shellcheck@2.0.0` action
       with an inline script that is runnable locally on both Linux and macOS;
    
    2. Ensures that all script files in the repository are subject to linting
       regardless of their shebang;
    
    3. Uses `shellcheck` that comes preinstalled in GitHub Actions runners
       rather than downloading it every time;
    
    4. Reformats shellcheck output with Actions "workflow commands" syntax,
       which should lead to nicer formatting of linting errors in GitHub
       pull requests view.

---
+++ .github/workflows/lint.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+set -eu
+
+declare -a scan_files
+
+# Scan any "*.sh" and "*.bash" files.
+while IFS= read -r -d '' file; do
+    scan_files+=("$file")
+done < <(find . -type f \( -name '*.sh' -o -name '*.bash' \) -print0)
+
+perm_executable="/111"
+# BSD `find` on macOS does not support the slash syntax for permissions.
+[ "$(uname -s)" = "Darwin" ] && perm_executable="+111"
+
+# Scan any executable files that start with a shebang.
+while IFS= read -r -d '' file; do
+    [ "$(head -c2 "$file")" = '#!' ] || continue
+    scan_files+=("$file")
+done < <(find . -type f ! -name '*.*' -perm "$perm_executable" -print0)
+
+# Convert shellcheck "gcc" format to GitHub Actions annotations.
+# https://docs.github.com/en/actions/writing-workflows/choosing-what-your-workflow-does/workflow-commands-for-github-actions#setting-an-error-message
+annotate_shellcheck() {
+    local file lineno _ severity message
+    while IFS=: read -r file lineno _ severity message; do
+        case "${severity# }" in
+        error ) severity="error" ;;
+        warning ) severity="warning" ;;
+        info | note | style ) severity="notice" ;;
+        esac
+        printf "::%s file=%s,line=%d::%s\n" "$severity" "$file" "$lineno" "${message# }"
+    done
+}
+
+if [ "$1" = "-l" ]; then
+    # Print only the list of files where shellcheck has detected violations.
+    shellcheck --format=gcc "${scan_files[@]}" | cut -d: -f1 | sort -u
+elif [ -z "$GITHUB_ACTIONS" ]; then
+    shellcheck "${scan_files[@]}"
+else
+    shellcheck --version
+    echo
+    set -o pipefail
+    shellcheck --format=gcc "${scan_files[@]}" | annotate_shellcheck
+fi

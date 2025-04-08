50621c02f6aeafc612a44e0478f26b370d3e23b1
Sergiy Belozorov <rryk.ua@gmail.com>
2025-04-08 04:07:08 +0200

    Wait for TXT record to propagate

--- duckdns/rootfs/root/hooks.sh
+++ duckdns/rootfs/root/hooks.sh
@@ -32,6 +32,7 @@ deploy_challenge() {
     #   be found in the $TOKEN_FILENAME file.
 
     curl -s "https://www.duckdns.org/update?domains=$ALIAS&token=$SYS_TOKEN&txt=$TOKEN_VALUE"
+    timeout 60s bash -c -- "while ! dig -t txt _acme-challenge.$ALIAS | grep $TOKEN_VALUE > /dev/null; do sleep 5; done"
 }
 
 clean_challenge() {

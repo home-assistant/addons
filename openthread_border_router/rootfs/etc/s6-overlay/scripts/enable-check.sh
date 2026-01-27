5cd17a213354ac8c3c3c698204df3721b222bd00
puddly <32534428+puddly@users.noreply.github.com>
2026-01-27 02:08:59 -0500

    Add a beta toggle

--- openthread_border_router/rootfs/etc/s6-overlay/scripts/enable-check.sh
+++ openthread_border_router/rootfs/etc/s6-overlay/scripts/enable-check.sh
@@ -1,6 +1,28 @@
 #!/usr/bin/with-contenv bashio
 # shellcheck shell=bash
 # ==============================================================================
+# Select OTBR version and enable mDNSResponder for stable mode
+# ==============================================================================
+
+if bashio::config.true 'beta'; then
+    bashio::log.info "Beta mode enabled, using OpenThread built-in mDNS."
+
+    ln -sf "/opt/otbr-beta/sbin/otbr-agent" /usr/sbin/otbr-agent
+    ln -sf "/opt/otbr-beta/sbin/otbr-web" /usr/sbin/otbr-web
+    ln -sf "/opt/otbr-beta/sbin/ot-ctl" /usr/sbin/ot-ctl
+else
+    bashio::log.info "Stable mode, enabling mDNSResponder."
+
+    touch /etc/s6-overlay/s6-rc.d/user/contents.d/mdns
+    touch /etc/s6-overlay/s6-rc.d/otbr-agent/dependencies.d/mdns
+
+    ln -sf "/opt/otbr-stable/sbin/otbr-agent" /usr/sbin/otbr-agent
+    ln -sf "/opt/otbr-stable/sbin/otbr-web" /usr/sbin/otbr-web
+    ln -sf "/opt/otbr-stable/sbin/ot-ctl" /usr/sbin/ot-ctl
+    ln -sf "/opt/otbr-stable/sbin/mdnsd" /usr/sbin/mdnsd
+fi
+
+# ==============================================================================
 # Disable OTBR Web if necessary ports are not exposed
 # ==============================================================================
 

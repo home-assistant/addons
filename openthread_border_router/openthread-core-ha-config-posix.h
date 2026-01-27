2356fdaa41704ea2c85eec85a39e1a5cd41275cd
puddly <32534428+puddly@users.noreply.github.com>
2026-01-27 01:09:53 -0500

    Clean up Dockerfile

--- openthread_border_router/openthread-core-ha-config-posix.h
+++ openthread_border_router/openthread-core-ha-config-posix.h
@@ -56,4 +56,21 @@
  */
 #define OPENTHREAD_CONFIG_CHANNEL_MONITOR_AUTO_START_ENABLE 0
 
+/**
+ * @def OPENTHREAD_CONFIG_BORDER_AGENT_MESHCOP_SERVICE_BASE_NAME
+ *
+ * Specifies the base name to construct the service instance name used when advertising the mDNS `_meshcop._udp`
+ * service by the Border Agent.
+ *
+ * Applicable when the `OPENTHREAD_CONFIG_BORDER_AGENT_MESHCOP_SERVICE_ENABLE` feature is enabled.
+ *
+ * The name can also be configured using the `otBorderAgentSetMeshCoPServiceBaseName()` API at run-time.
+ *
+ * Per the Thread specification, the service instance should be a user-friendly name identifying the device model or
+ * product. A recommended format is "VendorName ProductName".
+ *
+ * The name MUST have a length less than or equal to `OT_BORDER_AGENT_MESHCOP_SERVICE_BASE_NAME_MAX_LENGTH` (47 chars).
+ */
+#define OPENTHREAD_CONFIG_BORDER_AGENT_MESHCOP_SERVICE_BASE_NAME "Home Assistant OpenThread Border Router "
+
 #endif /* OPENTHREAD_CORE_HA_CONFIG_POSIX_H_ */

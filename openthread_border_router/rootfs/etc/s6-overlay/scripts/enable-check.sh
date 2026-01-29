f86d008c47f16b1fb643e358de9baece347e66f1
puddly <32534428+puddly@users.noreply.github.com>
2026-01-28 20:00:52 -0500

    Switch to ingress

--- openthread_border_router/rootfs/etc/s6-overlay/scripts/enable-check.sh
+++ openthread_border_router/rootfs/etc/s6-overlay/scripts/enable-check.sh
@@ -23,18 +23,6 @@ else
 fi
 
 # ==============================================================================
-# Disable OTBR Web if necessary ports are not exposed
-# ==============================================================================
-
-if bashio::var.has_value "$(bashio::addon.port 8080)" \
-     && bashio::var.has_value "$(bashio::addon.port 8081)"; then
-    bashio::log.info "Web UI and REST API port are exposed, starting otbr-web."
-else
-    rm /etc/s6-overlay/s6-rc.d/user/contents.d/otbr-web
-    bashio::log.info "The otbr-web is disabled."
-fi
-
-# ==============================================================================
 # Enable socat-otbr-tcp service if needed
 # ==============================================================================
 

3af7b9f88a5f237391651e03e98c00cf8a4494fc
clowncracker <49218426+clowncracker@users.noreply.github.com>
2026-07-27 20:07:03 -0700

    Update enable-check.sh
    samba: gate service on network_discovery

--- samba/rootfs/etc/s6-overlay/scripts/enable-check.sh
+++ samba/rootfs/etc/s6-overlay/scripts/enable-check.sh
@@ -11,7 +11,7 @@ else
     bashio::log.info "Service nmbd disabled"
 fi
 
-if bashio::config.true 'wsdd'; then
+if bashio::config.true 'network_discovery'; then
     touch /etc/s6-overlay/s6-rc.d/user/contents.d/wsdd
     bashio::log.info "Service wsdd enabled"
 else
